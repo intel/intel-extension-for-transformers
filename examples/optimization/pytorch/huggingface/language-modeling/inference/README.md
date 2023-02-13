ea7f50bcc6f5cf778137c7df9cf81970c9bb8de5
mengfeil <mengfei.li@intel.com>
2023-02-12 19:24:40 -0800

    modify

--- examples/optimization/pytorch/huggingface/language-modeling/inference/README.md
+++ examples/optimization/pytorch/huggingface/language-modeling/inference/README.md
@@ -1,6 +1,6 @@
 Step-by-Step
 ============
-This document describes the step-by-step instructions to benchmark EleutherAI/gpt-j-6B on Intel® Xeon® with PyTorch and Intel® Extension for PyTorch.
+This document describes the step-by-step instructions to run GPT-J on 4th Gen Intel® Intel® Scalable Processor (SPR) with PyTorch and Intel® Extension for PyTorch.
 
 The script ```run_gptj.py``` is based on [EleutherAI/gpt-j-6B](https://huggingface.co/EleutherAI/gpt-j-6B) and provides inference benchmarking.
 
@@ -13,7 +13,7 @@ pip install torch==1.13.1 --extra-index-url https://download.pytorch.org/whl/cpu
 pip install intel_extension_for_pytorch=1.13.0
 pip install -r requirements.txt
 ```
-## Setup variable to speedup
+## Setup Environment Variables
 ```
 export KMP_BLOCKTIME=1
 export KMP_SETTINGS=1
@@ -29,7 +29,7 @@ export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:aut
 
 # Performance
 ```bash
-numactl -m <node N> -C <cpu list> python run_gptj.py
+numactl -m <node N> -C <cpu list> python run_gptj.py <fp32/bf16>
 ```
 
 >**Note:** Inference performance speedup with Intel DL Boost (VNNI/AMX) on Intel(R) Xeon(R) hardware, Please refer to [Performance Tuning Guide](https://intel.github.io/intel-extension-for-pytorch/cpu/latest/tutorials/performance_tuning/tuning_guide.html) for more optimizations.
--- examples/optimization/pytorch/huggingface/language-modeling/inference/run_gptj.py
+++ examples/optimization/pytorch/huggingface/language-modeling/inference/run_gptj.py
@@ -3,31 +3,40 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import intel_extension_for_pytorch as ipex
 import time
+import sys
+
+
+# set args
+if len(sys.argv) > 1 and sys.argv[1] not in ['fp32', 'float32']:
+    amp_enabled = True
+    amp_dtype = torch.bfloat16
+else:
+    amp_enabled = False
+    amp_dtype = None
 
 # load model
 model_id = "EleutherAI/gpt-j-6B"
 model = AutoModelForCausalLM.from_pretrained(model_id, low_cpu_mem_usage=True)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-# model = model.to('cuda')
 model = model.eval()
 
 # to channels last
 model = model.to(memory_format=torch.channels_last)
 # to ipex
-model = ipex.optimize(model, dtype=torch.bfloat16, inplace=True)
+model = ipex.optimize(model, dtype=amp_dtype, inplace=True)
 
 # input prompt
 prompt = "Once upon a time,"
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 
 # start
 elapsed = time.time()
-with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
+with torch.cpu.amp.autocast(enabled=amp_enabled, dtype=amp_dtype):
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
     gen_tokens = model.generate(input_ids, do_sample=True,
                                 temperature=0.9, max_length=32, num_beams=1)
     gen_text = tokenizer.batch_decode(gen_tokens)[0]
 elapsed = time.time() - elapsed
 
 print(gen_text)
-print("inference Latency: %.3f ms." % (elapsed * 1000))
+print("Elapsed Time: %.3f ms." % (elapsed * 1000))
 
