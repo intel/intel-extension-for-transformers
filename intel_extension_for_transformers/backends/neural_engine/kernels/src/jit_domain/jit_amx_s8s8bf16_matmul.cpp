ba5eda8f32a4215b04e9bbdfd78ebaf239ab8e13
VincyZhang <wenxin.zhang@intel.com>
2023-04-12 21:46:15 +0800

    Sync with internal repo (#61)

--- intel_extension_for_transformers/backends/neural_engine/kernels/src/jit_domain/jit_amx_s8s8bf16_matmul.cpp
+++ intel_extension_for_transformers/backends/neural_engine/kernels/src/jit_domain/jit_amx_s8s8bf16_matmul.cpp
@@ -13,6 +13,9 @@
 //  limitations under the License.
 
 #include "jit_domain/jit_amx_s8s8bf16_matmul.hpp"
+
+#include "regs_pool.hpp"
+
 namespace jd {
 
 #define GET_OFF(field) offsetof(ssd::dynamic_quant_matmul_data_t, field)
@@ -23,51 +26,44 @@ void jit_amx_s8s8bf16_matmul_t::generate() {
   inLocalLabel();
   {
     const int cfg_size = sizeof(tileconfig_t);
-    const int stack_buf_size = 2 * cfg_size;
-    Xbyak::util::StackFrame st(this, 1, 13, stack_buf_size);
-    int reg_idx = 0;
-    const Reg64& reg_param = st.p[0];
-    const Reg64& reg_m_loop = st.t[reg_idx++];
-    const Reg64& reg_n_loop = st.t[reg_idx++];
-    const Reg64& reg_strideA = st.t[reg_idx++];
-    const Reg64& reg_strideB = st.t[reg_idx++];
-    const Reg64& reg_strideTmpbuf = st.t[reg_idx++];
-    const Reg64& reg_tmpbuf = st.t[reg_idx++];
-    const Reg64& reg_scale_w = st.t[reg_idx++];
-    const Reg64& reg_scale_a = st.t[reg_idx++];
-    const Reg64& reg_bias = st.t[reg_idx++];
-    const Reg64& reg_dst = st.t[reg_idx++];
+    regs_pool rp(this, 1, {13, 4, 0});
 
-    auto allocate_reg = [&] {
-      Reg64* reg = &const_cast<Reg64&>(st.t[reg_idx++]);
-      return std::shared_ptr<Reg64>(reg, [&](...) { reg_idx--; });
-    };
+    const auto reg_m_loop = rp.reg<Reg64>();
+    const auto reg_n_loop = rp.reg<Reg64>();
+    const auto reg_strideA = rp.reg<Reg64>();
+    const auto reg_strideB = rp.reg<Reg64>();
+    const auto reg_strideTmpbuf = rp.reg<Reg64>();
+    const auto reg_tmpbuf = rp.reg<Reg64>();
+    const auto reg_scale_w = rp.reg<Reg64>();
+    const auto reg_scale_a = rp.reg<Reg64>();
+    const auto reg_bias = rp.reg<Reg64>();
+    const auto reg_dst = rp.reg<Reg64>();
 
     auto prepare_mask = [&]() {
-      auto reg_tmp = allocate_reg();
-      mov(reg_tmp->cvt32(), 0xffff >> param_.write_mask);
-      kmovd(matC_n_mask_, reg_tmp->cvt32());
+      const auto reg_tmp = rp.reg<Xbyak::Reg32>();
+      mov(reg_tmp, 0xffff >> param_.write_mask);
+      kmovd(matC_n_mask_, reg_tmp);
     };
 
     auto ip_Mx16 = [&](int M, int block_num, bool need_mask = false, int no_mask_tail_n = 0) {
-      auto reg_tmp = allocate_reg();
+      const auto reg_tmp = rp.reg<Reg64>();
       // build block
       {
-        auto reg_matA_addr = allocate_reg();
-        auto reg_matB_addr = allocate_reg();
+        const auto reg_matA_addr = rp.reg<Reg64>();
+        const auto reg_matB_addr = rp.reg<Reg64>();
         for (int i = 0; i < block_num; i++) tilezero(Tmm(i));
         // prepare addr & stride;
-        mov(*reg_matA_addr, ptr[reg_param + GET_OFF(activation)]);
-        mov(*reg_matB_addr, ptr[reg_param + GET_OFF(reordered_weight)]);
-        imul(*reg_tmp, reg_m_loop, 16 * param_.k);
-        add(*reg_matA_addr, *reg_tmp);
-        imul(*reg_tmp, reg_n_loop, param_.align_build_block_num * trans_block_col * 64 * (param_.tile_k / 4));
-        add(*reg_matB_addr, *reg_tmp);
+        mov(reg_matA_addr, ptr[rp.p[0] + GET_OFF(activation)]);
+        mov(reg_matB_addr, ptr[rp.p[0] + GET_OFF(reordered_weight)]);
+        imul(reg_tmp, reg_m_loop, 16 * param_.k);
+        add(reg_matA_addr, reg_tmp);
+        imul(reg_tmp, reg_n_loop, param_.align_build_block_num * trans_block_col * 64 * (param_.tile_k / 4));
+        add(reg_matB_addr, reg_tmp);
         for (int k_loop = 0; k_loop < param_.k / param_.tile_k; k_loop++) {
-          tileloadd(Tmm(3), ptr[*reg_matA_addr + reg_strideA + k_loop * param_.tile_k]);
+          tileloadd(Tmm(3), ptr[reg_matA_addr + reg_strideA + k_loop * param_.tile_k]);
           for (int idx = 0; idx < block_num; idx++) {
             int offset = (idx + no_mask_tail_n) * trans_block_col * 64 * (param_.tile_k / 4) + k_loop * 64;
-            tileloadd(Tmm(4 + idx), ptr[*reg_matB_addr + reg_strideB + offset]);
+            tileloadd(Tmm(4 + idx), ptr[reg_matB_addr + reg_strideB + offset]);
             tdpbssd(Tmm(idx), Tmm(3), Tmm(4 + idx));
           }
         }
@@ -77,21 +73,21 @@ void jit_amx_s8s8bf16_matmul_t::generate() {
         for (int idx = 0; idx < block_num; idx++)
           tilestored(ptr[reg_tmpbuf + reg_strideTmpbuf + idx * 16 * sizeof(int)], Tmm(idx));
         // dequant + add_bias
-        auto zmms = regs<Zmm, 4>(0);
-        auto reg_tmp2 = allocate_reg();
-        auto reg_dst_offset = allocate_reg();
+        const auto zmms = rp.regs<Zmm, 4>();
+        const auto reg_tmp2 = rp.reg<Reg64>();
+        const auto reg_dst_offset = rp.reg<Reg64>();
 
-        imul(*reg_dst_offset, reg_m_loop, 16 * dst_n_dim_);
-        imul(*reg_tmp, reg_n_loop, param_.align_build_block_num * 16);
-        add(*reg_dst_offset, *reg_tmp);
-        imul(*reg_tmp, reg_n_loop, 16 * param_.align_build_block_num * sizeof(int));  // offset of scale_w & bias
-        imul(*reg_tmp2, reg_m_loop, 16 * sizeof(float));                              // offset of scale_a
+        imul(reg_dst_offset, reg_m_loop, 16 * dst_n_dim_);
+        imul(reg_tmp, reg_n_loop, param_.align_build_block_num * 16);
+        add(reg_dst_offset, reg_tmp);
+        imul(reg_tmp, reg_n_loop, 16 * param_.align_build_block_num * sizeof(int));  // offset of scale_w & bias
+        imul(reg_tmp2, reg_m_loop, 16 * sizeof(float));                              // offset of scale_a
         for (int idx = 0; idx < block_num; idx++) {
-          vmovups(zmms[0], ptr[reg_scale_w + *reg_tmp + (idx + no_mask_tail_n) * 16 * sizeof(float)]);
-          if (param_.add_bias) vmovups(zmms[1], ptr[reg_bias + *reg_tmp + (idx + no_mask_tail_n) * 16 * sizeof(float)]);
+          vmovups(zmms[0], ptr[reg_scale_w + reg_tmp + (idx + no_mask_tail_n) * 16 * sizeof(float)]);
+          if (param_.add_bias) vmovups(zmms[1], ptr[reg_bias + reg_tmp + (idx + no_mask_tail_n) * 16 * sizeof(float)]);
           for (int row_loop = 0; row_loop < M; row_loop++) {
             vcvtdq2ps(zmms[2], ptr[reg_tmpbuf + (idx + row_loop * param_.align_build_block_num) * 16 * sizeof(float)]);
-            vbroadcastss(zmms[3], dword[reg_scale_a + *reg_tmp2 + row_loop * sizeof(float)]);
+            vbroadcastss(zmms[3], dword[reg_scale_a + reg_tmp2 + row_loop * sizeof(float)]);
             vmulps(zmms[2], zmms[2], zmms[3]);
             if (param_.add_bias)
               vfmadd213ps(zmms[2], zmms[0], zmms[1]);
@@ -99,7 +95,7 @@ void jit_amx_s8s8bf16_matmul_t::generate() {
               vmulps(zmms[2], zmms[2], zmms[0]);
 
             fp32_cvt_bf16(zmms[2]);
-            RegExp write_back_addr = reg_dst + *reg_dst_offset * sizeof(bfloat16_t) +
+            RegExp write_back_addr = reg_dst + reg_dst_offset * sizeof(bfloat16_t) +
                                      ((no_mask_tail_n + idx) * 16 + row_loop * dst_n_dim_) * sizeof(bfloat16_t);
             vmovdqu16(need_mask ? ptr[write_back_addr] | matC_n_mask_ : ptr[write_back_addr], Ymm(zmms[2].getIdx()));
           }
@@ -134,11 +130,11 @@ void jit_amx_s8s8bf16_matmul_t::generate() {
     mov(reg_strideA, param_.k);
     mov(reg_strideB, trans_block_col * 64);
     mov(reg_strideTmpbuf, 16 * param_.align_build_block_num * sizeof(int));
-    mov(reg_tmpbuf, ptr[reg_param + GET_OFF(tmp_buf)]);
-    mov(reg_scale_a, ptr[reg_param + GET_OFF(scale_a)]);
-    mov(reg_scale_w, ptr[reg_param + GET_OFF(scale_w)]);
-    mov(reg_bias, ptr[reg_param + GET_OFF(bias)]);
-    mov(reg_dst, ptr[reg_param + GET_OFF(dst)]);
+    mov(reg_tmpbuf, ptr[rp.p[0] + GET_OFF(tmp_buf)]);
+    mov(reg_scale_a, ptr[rp.p[0] + GET_OFF(scale_a)]);
+    mov(reg_scale_w, ptr[rp.p[0] + GET_OFF(scale_w)]);
+    mov(reg_bias, ptr[rp.p[0] + GET_OFF(bias)]);
+    mov(reg_dst, ptr[rp.p[0] + GET_OFF(dst)]);
 
     if (param_.align_m_loop > 0) {
       ldtilecfg(ptr[rip + cfg_label]);
