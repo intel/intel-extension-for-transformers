ab89f0c1af1a19c2ca043d5ea8ff46aca25ab266
mengfeil <mengfei.li@intel.com>
2023-05-11 01:50:14 -0700

    add example for llm text generation for ipex and torch

---
+++ examples/huggingface/pytorch/text-generation/inference/README.md
@@ -0,0 +1,44 @@
+# GPT-J
+We provide the inference benchmarking script `run_generation.py` for large language models text generation.
+
+## Setup
+```bash
+WORK_DIR=$PWD
+# Create Environment (conda recommended)
+conda create -n llm python=3.9 -y
+conda install mkl mkl-include -y
+conda install gperftools -c conda-forge -y
+
+# install PyTorch
+git clone https://github.com/pytorch/pytorch
+cd pytorch
+git submodule sync
+git submodule update --init --recursive
+python setup.py install
+cd ../
+# install
+git clone https://github.com/intel/intel-extension-for-pytorch
+cd intel-extension-for-pytorch
+git submodule sync
+git submodule update --init --recursive
+python setup.py install
+cd ../
+# install others deps
+pip install transformers accelerate onnx
+pip install --no-deps optimum
+pip install --no-deps git+https://github.com/huggingface/optimum-intel.git@main
+
+```
+## Performance
+```bash
+# Setup Environment Variables for best performance on Xeon
+export KMP_BLOCKTIME=1
+export KMP_SETTINGS=1
+export KMP_AFFINITY=granularity=fine,compact,1,0
+export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libiomp5.so # Intel OpenMP
+# Tcmalloc is a recommended malloc implementation that emphasizes fragmentation avoidance and scalable concurrency support.
+export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libtcmalloc.so
+
+# support single socket and multiple sockets
+OMP_NUM_THREADS=<physical cores num> numactl -m <node N> -C <physical cores list> python run_generation.py --device cpu --ipex --jit --dtype bfloat16 --max-new-tokens 32
+```
---
+++ examples/huggingface/pytorch/text-generation/inference/run_generation.py
@@ -0,0 +1,94 @@
+import torch
+import time
+import argparse
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+# args
+parser = argparse.ArgumentParser("Text generation script", add_help=False)
+parser.add_argument(
+    "-m", "--model-id", type=str, required=True, help="huggingface model id"
+)
+parser.add_argument("--dtype", type=str, default="float32", help="bfloat16 or float32")
+parser.add_argument("--max-new-tokens", default=32, type=int, help="max new tokens")
+parser.add_argument("--ipex", action="store_true", help="Use IPEX")
+parser.add_argument("--batch-size", default=1, type=int, help="batch size")
+args = parser.parse_args()
+print(args)
+
+
+# device
+device = torch.device("cpu")
+
+if args.ipex:
+    from optimum.intel import inference_mode as ipex_inference_mode
+    torch._C._jit_set_texpr_fuser_enabled(False)
+
+# torch dtype
+if args.dtype == "bfloat16":
+    amp_enabled = True
+    amp_dtype = torch.bfloat16
+else:
+    amp_enabled = False
+    amp_dtype = torch.float32
+
+# load model
+model_id = args.model_id
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    low_cpu_mem_usage=True,
+    return_dict=not args.ipex,
+    torch_dtype=amp_dtype
+)
+print("model dtype:", model.config.torch_dtype)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = model.eval().to(device)
+model = model.to(memory_format=torch.channels_last)
+
+# generate args
+generate_kwargs = dict(
+    do_sample=False, temperature=0.9, num_beams=4, max_new_tokens=args.max_new_tokens
+)
+text_generator = pipeline(
+    "text-generation", model=model, tokenizer=tokenizer, **generate_kwargs
+)
+
+# input prompt
+prompt = "Develop a C++ program that reads a text file line by line and counts the number of occurrences of a specific word in the file"
+prompt = [prompt] * args.batch_size
+input_size = tokenizer(prompt, return_tensors="pt").input_ids.size(dim=1)
+print("---- Prompt size:", input_size)
+
+# start
+num_iter = 10
+num_warmup = 3
+def torch_generation():
+    total_time = 0.0
+    with torch.inference_mode(), torch.autocast('cpu', enabled=amp_enabled, dtype=amp_dtype if amp_enabled else None):
+        for i in range(num_iter):
+            tic = time.time()
+            output = text_generator(prompt)
+            toc = time.time()
+            print(output, flush=True)
+            if i >= num_warmup:
+                total_time += toc - tic
+    return total_time, num_iter - num_warmup
+
+def ipex_generation():
+    total_time = 0.0
+    with ipex_inference_mode(text_generator, dtype=amp_dtype, jit=True) as ipex_text_generator:
+        for i in range(num_iter):
+            tic = time.time()
+            output = ipex_text_generator(prompt)
+            toc = time.time()
+            print(output, flush=True)
+            if i >= num_warmup:
+                total_time += toc - tic
+    return total_time, num_iter - num_warmup
+
+total_time, total_iter = ipex_generation() if args.ipex else torch_generation()
+latency = total_time / total_iter
+print(
+    "\nSentence latency: %.3f sec, Token latency: %.3f sec, Token throughput: %.2f tokens/s.\n"
+    % (latency, latency / args.max_new_tokens, args.max_new_tokens / latency)
+)
