970f253891d9997c22fc3c72b31defa51753d36e
Meng, Hengyu <hengyu.meng@intel.com>
2023-07-11 19:53:26 +0800

    [Graph] verify file names and format (#1150)

--- intel_extension_for_transformers/backends/neural_engine/graph/application/ChatFALCON/quant_falcon.cpp
+++ intel_extension_for_transformers/backends/neural_engine/graph/application/ChatFALCON/quant_falcon.cpp
@@ -27,152 +27,157 @@
 
 // default hparams (Falcon 7B)
 struct falcon_hparams {
-    int32_t n_vocab = 65024;
-    int32_t n_embd  = 4544;
-    int32_t n_head  = 71;
-    int32_t n_layer = 32;
-    int32_t ftype   = 1;
+  int32_t n_vocab = 65024;
+  int32_t n_embd = 4544;
+  int32_t n_head = 71;
+  int32_t n_layer = 32;
+  int32_t ftype = 1;
 };
 
 // quantize a model
-bool falcon_model_quantize(const std::string & fname_inp, const std::string & fname_out, ne_ftype ftype) {
-    gpt_vocab vocab;
+bool falcon_model_quantize(const std::string& fname_inp, const std::string& fname_out, ne_ftype ftype) {
+  gpt_vocab vocab;
 
-    printf("%s: loading model from '%s'\n", __func__, fname_inp.c_str());
+  printf("%s: loading model from '%s'\n", __func__, fname_inp.c_str());
 
-    auto finp = std::ifstream(fname_inp, std::ios::binary);
-    if (!finp) {
-        fprintf(stderr, "%s: failed to open '%s' for reading\n", __func__, fname_inp.c_str());
-        return false;
+  auto finp = std::ifstream(fname_inp, std::ios::binary);
+  if (!finp) {
+    fprintf(stderr, "%s: failed to open '%s' for reading\n", __func__, fname_inp.c_str());
+    return false;
+  }
+
+  auto fout = std::ofstream(fname_out, std::ios::binary);
+  if (!fout) {
+    fprintf(stderr, "%s: failed to open '%s' for writing\n", __func__, fname_out.c_str());
+    return false;
+  }
+
+  // verify magic
+  {
+    uint32_t magic;
+    finp.read((char*)&magic, sizeof(magic));
+    if (magic != 0x67676d6c) {
+      fprintf(stderr, "%s: invalid model file '%s' (bad magic)\n", __func__, fname_inp.c_str());
+      return false;
     }
 
-    auto fout = std::ofstream(fname_out, std::ios::binary);
-    if (!fout) {
-        fprintf(stderr, "%s: failed to open '%s' for writing\n", __func__, fname_out.c_str());
-        return false;
+    fout.write((char*)&magic, sizeof(magic));
+  }
+
+  falcon_hparams hparams;
+
+  // load hparams
+  {
+    finp.read((char*)&hparams.n_vocab, sizeof(hparams.n_vocab));
+    finp.read((char*)&hparams.n_embd, sizeof(hparams.n_embd));
+    finp.read((char*)&hparams.n_head, sizeof(hparams.n_head));
+    finp.read((char*)&hparams.n_layer, sizeof(hparams.n_layer));
+    finp.read((char*)&hparams.ftype, sizeof(hparams.ftype));
+
+    const int32_t qntvr_src = hparams.ftype / NE_QNT_VERSION_FACTOR;
+    const int32_t ftype_dst = NE_QNT_VERSION * NE_QNT_VERSION_FACTOR + ftype;
+
+    printf("%s: n_vocab     = %d\n", __func__, hparams.n_vocab);
+    printf("%s: n_embd      = %d\n", __func__, hparams.n_embd);
+    printf("%s: n_head      = %d\n", __func__, hparams.n_head);
+    printf("%s: n_layer     = %d\n", __func__, hparams.n_layer);
+    printf("%s: ftype (src) = %d\n", __func__, hparams.ftype);
+    printf("%s: qntvr (src) = %d\n", __func__, qntvr_src);
+    printf("%s: ftype (dst) = %d\n", __func__, ftype_dst);
+    printf("%s: qntvr (dst) = %d\n", __func__, NE_QNT_VERSION);
+
+    fout.write((char*)&hparams.n_vocab, sizeof(hparams.n_vocab));
+    fout.write((char*)&hparams.n_embd, sizeof(hparams.n_embd));
+    fout.write((char*)&hparams.n_head, sizeof(hparams.n_head));
+    fout.write((char*)&hparams.n_layer, sizeof(hparams.n_layer));
+    fout.write((char*)&ftype_dst, sizeof(ftype_dst));
+  }
+
+  // load vocab
+  {
+    const int32_t n_vocab = hparams.n_vocab;
+
+    std::string word;
+    for (int i = 0; i < n_vocab; i++) {
+      uint32_t len;
+      finp.read((char*)&len, sizeof(len));
+      fout.write((char*)&len, sizeof(len));
+
+      word.resize(len);
+      finp.read((char*)word.data(), len);
+      fout.write((char*)word.data(), len);
+
+      vocab.token_to_id[word] = i;
+      vocab.id_to_token[i] = word;
     }
+  }
 
-    // verify magic
-    {
-        uint32_t magic;
-        finp.read((char *) &magic, sizeof(magic));
-        if (magic != 0x67676d6c) {
-            fprintf(stderr, "%s: invalid model file '%s' (bad magic)\n", __func__, fname_inp.c_str());
-            return false;
-        }
+  // regexes of tensor names to be quantized
+  const std::vector<std::string> to_quant = {
+      ".*weight",
+  };
 
-        fout.write((char *) &magic, sizeof(magic));
-    }
+  if (!ne_common_quantize_0(finp, fout, ftype, to_quant, {})) {
+    fprintf(stderr, "%s: failed to quantize model '%s'\n", __func__, fname_inp.c_str());
+    return false;
+  }
 
-    falcon_hparams hparams;
+  finp.close();
+  fout.close();
 
-    // load hparams
-    {
-        finp.read((char *) &hparams.n_vocab, sizeof(hparams.n_vocab));
-        finp.read((char *) &hparams.n_embd,  sizeof(hparams.n_embd));
-        finp.read((char *) &hparams.n_head,  sizeof(hparams.n_head));
-        finp.read((char *) &hparams.n_layer, sizeof(hparams.n_layer));
-        finp.read((char *) &hparams.ftype,   sizeof(hparams.ftype));
-
-        const int32_t qntvr_src =    hparams.ftype / NE_QNT_VERSION_FACTOR;
-        const int32_t ftype_dst = NE_QNT_VERSION * NE_QNT_VERSION_FACTOR + ftype;
-
-        printf("%s: n_vocab     = %d\n", __func__, hparams.n_vocab);
-        printf("%s: n_embd      = %d\n", __func__, hparams.n_embd);
-        printf("%s: n_head      = %d\n", __func__, hparams.n_head);
-        printf("%s: n_layer     = %d\n", __func__, hparams.n_layer);
-        printf("%s: ftype (src) = %d\n", __func__, hparams.ftype);
-        printf("%s: qntvr (src) = %d\n", __func__, qntvr_src);
-        printf("%s: ftype (dst) = %d\n", __func__, ftype_dst);
-        printf("%s: qntvr (dst) = %d\n", __func__, NE_QNT_VERSION);
-
-        fout.write((char *) &hparams.n_vocab, sizeof(hparams.n_vocab));
-        fout.write((char *) &hparams.n_embd,  sizeof(hparams.n_embd));
-        fout.write((char *) &hparams.n_head,  sizeof(hparams.n_head));
-        fout.write((char *) &hparams.n_layer, sizeof(hparams.n_layer));
-        fout.write((char *) &ftype_dst,       sizeof(ftype_dst));
-    }
-
-    // load vocab
-    {
-        const int32_t n_vocab = hparams.n_vocab;
-
-        std::string word;
-        for (int i = 0; i < n_vocab; i++) {
-            uint32_t len;
-            finp.read ((char *) &len, sizeof(len));
-            fout.write((char *) &len, sizeof(len));
-
-            word.resize(len);
-            finp.read ((char *) word.data(), len);
-            fout.write((char *) word.data(), len);
-
-            vocab.token_to_id[word] = i;
-            vocab.id_to_token[i] = word;
-        }
-    }
-
-    // regexes of tensor names to be quantized
-    const std::vector<std::string> to_quant = {
-        ".*weight",
-    };
-
-    if (!ne_common_quantize_0(finp, fout, ftype, to_quant, {})) {
-        fprintf(stderr, "%s: failed to quantize model '%s'\n", __func__, fname_inp.c_str());
-        return false;
-    }
-
-    finp.close();
-    fout.close();
-
-    return true;
+  return true;
 }
 
 // usage:
 //  ./quant_falcon models/ne-model.bin models/ne-model-quant.bin type (q8_0 / q4_0, etc.)
-int main(int argc, char ** argv) {
-    if (argc != 4) {
-        fprintf(stderr, "usage: %s model-f32.bin model-quant.bin type\n", argv[0]);
-        ne_print_ftypes(stderr);
-        return 1;
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    fprintf(stderr, "usage: %s model-f32.bin model-quant.bin type\n", argv[0]);
+    ne_print_ftypes(stderr);
+    return 1;
+  }
+
+  // needed to initialize f16 tables
+  {
+    struct ne_init_params params = {0, NULL, false};
+    struct ne_context* ctx = ne_init(params);
+    ne_free(ctx);
+  }
+
+  const std::string fname_inp = argv[1];
+  const std::string fname_out = argv[2];
+
+  if (!isValidFilename(fname_out) || !isValidFilename(fname_inp)) {
+    fprintf(stderr, "invalid file names '%s' '%s'\n", fname_out.c_str(), fname_inp.c_str());
+    return 1;
+  }
+
+  const ne_ftype ftype = ne_parse_ftype(argv[3]);
+
+  const int64_t t_main_start_us = ne_time_us();
+
+  int64_t t_quantize_us = 0;
+
+  // load the model
+  {
+    const int64_t t_start_us = ne_time_us();
+
+    if (!falcon_model_quantize(fname_inp, fname_out, ne_ftype(ftype))) {
+      fprintf(stderr, "%s: failed to quantize model from '%s'\n", __func__, fname_inp.c_str());
+      return 1;
     }
 
-    // needed to initialize f16 tables
-    {
-        struct ne_init_params params = { 0, NULL, false };
-        struct ne_context * ctx = ne_init(params);
-        ne_free(ctx);
-    }
+    t_quantize_us = ne_time_us() - t_start_us;
+  }
 
-    const std::string fname_inp = argv[1];
-    const std::string fname_out = argv[2];
+  // report timing
+  {
+    const int64_t t_main_end_us = ne_time_us();
 
-    const ne_ftype ftype = ne_parse_ftype(argv[3]);
+    printf("\n");
+    printf("%s: quantize time = %8.2f ms\n", __func__, t_quantize_us / 1000.0f);
+    printf("%s:    total time = %8.2f ms\n", __func__, (t_main_end_us - t_main_start_us) / 1000.0f);
+  }
 
-    const int64_t t_main_start_us = ne_time_us();
-
-    int64_t t_quantize_us = 0;
-
-    // load the model
-    {
-        const int64_t t_start_us = ne_time_us();
-
-        if (!falcon_model_quantize(fname_inp, fname_out, ne_ftype(ftype))) {
-            fprintf(stderr, "%s: failed to quantize model from '%s'\n", __func__, fname_inp.c_str());
-            return 1;
-        }
-
-        t_quantize_us = ne_time_us() - t_start_us;
-    }
-
-    // report timing
-    {
-        const int64_t t_main_end_us = ne_time_us();
-
-        printf("\n");
-        printf("%s: quantize time = %8.2f ms\n", __func__, t_quantize_us/1000.0f);
-        printf("%s:    total time = %8.2f ms\n", __func__, (t_main_end_us - t_main_start_us)/1000.0f);
-    }
-
-    return 0;
+  return 0;
 }
