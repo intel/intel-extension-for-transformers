0aeb4dd751312ac8a496f36e46f26314b3086dc5
ZheWang <zhe1.wang@intel.com>
2023-07-12 07:39:45 +0000

    delete packedw & more elegant exception

--- intel_extension_for_transformers/backends/bits_quantization/cscr/jblas_quantize.cpp
+++ intel_extension_for_transformers/backends/bits_quantization/cscr/jblas_quantize.cpp
@@ -38,53 +38,46 @@ torch::Tensor quant_launcher(const torch::Tensor& Fp32Wei, int64_t nthread,
                              int64_t bits, torch::string alg,
                              int64_t block_size, std::string scale_dtype,
                              std::string gemm_isa) {
-  try {
-    if (alg != "sym") throw std::runtime_error("unsupported alg.");
-    if (bits != 4 && bits != 8) throw std::runtime_error("bits must be 4/8");
-    if (scale_dtype != "fp32" && scale_dtype != "bf16")
-      throw std::runtime_error("scale_dtype must be fp32/bf16");
-    if (Fp32Wei.sizes().size() != 2)
-      throw std::runtime_error(
-          "dim of weight dosen't meet requirement, must be 2.");
-    int k_ = Fp32Wei.sizes()[0];
-    int n_ = Fp32Wei.sizes()[1];
-    using GemmKernel =
-        jblas::wrapper::gemm_default::weight_comp::avx512f::GemmKernelS4KBlock;
-    using GemmVnniKernel = jblas::wrapper::gemm_default::weight_comp::
-        avx512_vnni::GemmKernelDynamicQuantS4KBlock;
-    GemmKernel kernel;
-    GemmVnniKernel vnnikernel;
+  TORCH_CHECK(alg == "sym", "unsupported alg, only support sym currently.");
+  TORCH_CHECK(bits == 4 || bits != 8, "bits must be 4/8");
+  TORCH_CHECK(scale_dtype == "fp32" || scale_dtype != "bf16",
+              "scale_dtype must be fp32/bf16");
+  TORCH_CHECK(Fp32Wei.sizes().size() == 2,
+              "dim of weight dosen't meet requirement, must be 2.");
+  int k_ = Fp32Wei.sizes()[0];
+  int n_ = Fp32Wei.sizes()[1];
+  using GemmKernel =
+      jblas::wrapper::gemm_default::weight_comp::avx512f::GemmKernelS4KBlock;
+  using GemmVnniKernel = jblas::wrapper::gemm_default::weight_comp::
+      avx512_vnni::GemmKernelDynamicQuantS4KBlock;
+  GemmKernel kernel;
+  GemmVnniKernel vnnikernel;
+  jblas::prologue::PackedWeight* packedw = NULL;
 
-    jblas::prologue::PackedWeight* packedw = NULL;
-
-    auto cd = jblas::utils::parallel::CpuDevice::getInstance();
-    auto type = NE_FTYPE_MAP[std::make_tuple(bits, alg, scale_dtype)];
-    if (gemm_isa == "vnni") {
-      if (cd->AVX512F()) {
-        packedw =
-            vnnikernel.getWeightPtr()->compressWeightTranspose<JblasAVX512F>(
-                n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
-      } else {
-        packedw =
-            vnnikernel.getWeightPtr()->compressWeightTranspose<JblasNoSIMD>(
-                n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
-      }
+  auto cd = jblas::utils::parallel::CpuDevice::getInstance();
+  auto type = NE_FTYPE_MAP[std::make_tuple(bits, alg, scale_dtype)];
+  if (gemm_isa == "vnni") {
+    if (cd->AVX512F()) {
+      packedw =
+          vnnikernel.getWeightPtr()->compressWeightTranspose<JblasAVX512F>(
+              n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
     } else {
-      if (cd->AVX512F()) {
-        packedw = kernel.getWeightPtr()->compressWeightTranspose<JblasAVX512F>(
-            n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
-      } else {
-        packedw = kernel.getWeightPtr()->compressWeightTranspose<JblasNoSIMD>(
-            n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
-      }
+      packedw = vnnikernel.getWeightPtr()->compressWeightTranspose<JblasNoSIMD>(
+          n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
     }
-
-    auto tsize = packedw->getSerializedSize();
-    torch::Tensor output = torch::zeros(tsize, torch::kInt8);
-    packedw->serializeToBuffer(output.data_ptr<int8_t>());
-    return output;
-  } catch (const std::exception& e) {
-    std::cout << e.what() << std::endl;
-    assert(false);
+  } else {
+    if (cd->AVX512F()) {
+      packedw = kernel.getWeightPtr()->compressWeightTranspose<JblasAVX512F>(
+          n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
+    } else {
+      packedw = kernel.getWeightPtr()->compressWeightTranspose<JblasNoSIMD>(
+          n_, k_, Fp32Wei.data_ptr<float>(), k_, block_size, type);
+    }
   }
+
+  auto tsize = packedw->getSerializedSize();
+  torch::Tensor output = torch::zeros(tsize, torch::kInt8);
+  packedw->serializeToBuffer(output.data_ptr<int8_t>());
+  delete packedw;
+  return output;
 }
