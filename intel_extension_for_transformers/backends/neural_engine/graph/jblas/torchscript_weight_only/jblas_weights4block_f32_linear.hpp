b04f8cf71bd58e9ea8a8eca4f923bf66c591e14e
ZheWang <zhe1.wang@intel.com>
2023-07-11 20:23:43 -0700

    add s4block-f32-linear interface.

---
+++ intel_extension_for_transformers/backends/neural_engine/graph/jblas/torchscript_weight_only/jblas_weights4block_f32_linear.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <torch/script.h>
+
+torch::Tensor weights4block_f32_linear_launcher(torch::Tensor activation,
+                                                torch::Tensor weight, int64_t m,
+                                                int64_t n, int64_t k,
+                                                int64_t lda, int64_t ldo);
--- intel_extension_for_transformers/backends/neural_engine/graph/jblas/torchscript_weight_only/weight_only_jblasop.cpp
+++ intel_extension_for_transformers/backends/neural_engine/graph/jblas/torchscript_weight_only/weight_only_jblasop.cpp
@@ -2,6 +2,7 @@
 #include <torch/torch.h>
 
 #include "jblas_quantize.hpp"
+#include "jblas_weights4block_f32_linear.hpp"
 
 static torch::Tensor jblas_quantize(torch::Tensor Fp32Wei, int64_t nthread,
                                     int64_t bits, torch::string alg,
@@ -10,7 +11,17 @@ static torch::Tensor jblas_quantize(torch::Tensor Fp32Wei, int64_t nthread,
   return quant_launcher(Fp32Wei, nthread, bits, alg, block_size, scale_dtype,
                         gemm_isa);
 }
-// static auto registry = torch::RegisterOperators("myop::skbmm", &skbmm);
+
+static torch::Tensor jblas_weights4block_f32_linear(torch::Tensor activation,
+                                                    torch::Tensor weight,
+                                                    int64_t m, int64_t n,
+                                                    int64_t k, int64_t lda,
+                                                    int64_t ldo) {
+  return weights4block_f32_linear_launcher(activation, weight, m, n, k, lda,
+                                           ldo);
+}
+
 TORCH_LIBRARY(weight_only_jblasop, m) {
   m.def("jblas_quantize", &jblas_quantize);
+  m.def("jblas_weights4block_f32_linear", &jblas_weights4block_f32_linear);
 }
