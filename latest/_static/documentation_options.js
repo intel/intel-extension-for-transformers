61a28049b1f9fefb5df8664823db9734e626d864
VincyZhang <VincyZhang@users.noreply.github.com>
2023-07-18 03:16:45 +0000

    deploy: 5c5fcfe4a40c116e4f05f9de3bc24faff6b1cf18

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g4269f96',
+    VERSION: '0.1.dev1+g5c5fcfe',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
