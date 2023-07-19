4f0466b6e83877eca6a22c30d56516b575654445
Samanway Sadhu <samanwaysadhu5@gmail.com>
2023-07-18 19:06:41 -0700

    Update cat parser (#1192)
    * Update README.md
    
    * Added multinode config
    
    * added parser args to run.py
    
    * updated parser args

--- workflows/compression_aware_training/src/run.py
+++ workflows/compression_aware_training/src/run.py
@@ -13,13 +13,19 @@
 # and limitations under the License.
 #
 
-import sys
+import argparse
 from itrex_opt import ItrexOpt
 
 
 def main():
-    args = sys.argv
-    itrex_opt = ItrexOpt(args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", type = str, required = True)
+    parser.add_argument("--local-rank", type = int, required = False)
+    parser.add_argument("--local_rank", type = int, required = False)
+    parser.add_argument("--no_cuda", action = 'store_true', required = False, default = False)
+    args = parser.parse_args()
+
+    itrex_opt = ItrexOpt(args.config_file, args.no_cuda)
     itrex_opt.e2e()
 
 
