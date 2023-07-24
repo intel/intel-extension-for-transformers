1b44631cec8df595d7eb31dca4e88ff7d79ae0b7
lvliang-intel <liang1.lv@intel.com>
2023-07-24 10:52:53 +0800

    Support query cache for chatbot (#1148)

--- workflows/chatbot/inference/backend/fastrag/fastrag_service.py
+++ workflows/chatbot/inference/backend/fastrag/fastrag_service.py
@@ -21,19 +21,33 @@ from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI, HuggingFacePipeline
 from langchain.chains.question_answering import load_qa_chain
 from langchain.text_splitter import CharacterTextSplitter
-from transformers import AutoModelForCausalLM, AutoModel, AutoTokenizer, pipeline
+from langchain.chains import RetrievalQA
+from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer, StoppingCriteria, StoppingCriteriaList, GenerationConfig
 from logger import build_logger
 from haystack.nodes import MarkdownConverter, TextConverter
 from utils import detect_language
+from starlette.responses import RedirectResponse
+
+logger = build_logger("fastrag_service", f"fastrag_service.log")
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", type=str, default="0.0.0.0")
+parser.add_argument("--port", type=int, default=80)
+parser.add_argument("--model-path", type=str, default="mosaicml/mpt-7b-chat")
+parser.add_argument(
+    "--cache-chat-config-file", default="cache_config.yml", help="the cache config file"
+)
+parser.add_argument(
+    "--cache-embedding-model-dir", default="./instructor-large", help="the cache embedding model directory"
+)
+args = parser.parse_args()
+logger.info(f"args: {args}")
 
 def ask_gm_documents_dense_embedding(folder_path, process_content=False):
+    documents = []
     for dirpath, dirnames, filenames in os.walk(folder_path):
         for filename in filenames:
-            # print(os.path.join(dirpath, filename))
-            if filename.endswith(".jsonl"):
-                documents = doc_index.d_load_jsonl_file(os.path.join(dirpath, filename), process_content)
-            elif filename.endswith(".pdf") or filename.endswith(".docx") or filename.endswith(".txt"):
-                documents = doc_index.d_load_file(os.path.join(dirpath, filename), process_content)
+            if filename.endswith(".json"):
+                documents = doc_index.d_load_jsonl_file(os.path.join(dirpath, filename), process_content, documents)
             elif filename.endswith(".xlsx"):
                 documents = doc_index.d_load_xlsx(os.path.join(dirpath, filename), process_content)
             else:
@@ -47,14 +61,10 @@ def ask_gm_documents_dense_embedding(folder_path, process_content=False):
 def ask_gm_documents_sparse_embedding(folder_path, process_content=False):
     document_store = ElasticsearchDocumentStore(host="localhost", index="elastic_askgm_sparse",
                         port=9200, search_fields=["content", "title"])
-
     for dirpath, dirnames, filenames in os.walk(folder_path):
         for filename in filenames:
-            if filename.endswith("jsonl"):
+            if filename.endswith("json"):
                 document_store = doc_index.s_load_jsonl_file(
-                    os.path.join(dirpath, filename), process_content, document_store)
-            elif filename.endswith("pdf") or filename.endswith("docx") or filename.endswith("txt"):
-                document_store = doc_index.s_load_file(
                     os.path.join(dirpath, filename), process_content, document_store)
             elif filename.endswith(".xlsx"):
                 document_store = doc_index.s_load_xlsx(
@@ -68,24 +78,15 @@ es = Elasticsearch(hosts=["localhost:9200"])
 index_name = "elastic_askgm_sparse"
 index_exists = es.indices.exists(index=index_name)
 if not index_exists:
-    ask_gm_documents_sparse_embedding("./doc/")
+    ask_gm_documents_sparse_embedding("./doc/ask_gm/", True)
 
 if not os.path.exists("/tmp/ask_gm_dense_retrieval_chinese") and \
    not os.path.exists("/tmp/ask_gm_dense_retrieval_english"):
-    ask_gm_documents_dense_embedding("./doc/")
+    ask_gm_documents_dense_embedding("./doc/ask_gm/", True)
 
 inc_document_store = InMemoryDocumentStore(use_gpu=False, use_bm25=True)
 es_document_store = ElasticsearchDocumentStore(host="localhost", index="elastic_index_1",
                                                port=9200, search_fields=["content", "title"])
-
-logger = build_logger("fastrag_service", f"fastrag_service.log")
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--host", type=str, default="0.0.0.0")
-parser.add_argument("--port", type=int, default=80)
-parser.add_argument("--model-path", type=str, default="mosaicml/mpt-7b-chat")
-args = parser.parse_args()
-logger.info(f"args: {args}")
 
 from haystack.schema import Document
 
@@ -105,7 +106,7 @@ from mpt_invocation import MptPromptModel
 inc_retriever = BM25Retriever(document_store=inc_document_store, top_k = 10)
 es_retriever = BM25Retriever(document_store=es_document_store, top_k = 10)
 
-reranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco-MiniLM-L-6-v2", batch_size=32, top_k=5)
+reranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco-MiniLM-L-6-v2", batch_size=32, top_k=2)
 
 # FastRAG model wrapper
 #pmodel = FastRAGPromptModel(model_name_or_path=args.model_path)
@@ -113,15 +114,37 @@ reranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco
 pmodel = MptPromptModel(model_name_or_path=args.model_path)
 
 # langchain model
-langchain_tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-langchain_model = AutoModelForCausalLM.from_pretrained(args.model_path,
-                                                 low_cpu_mem_usage=True,
-                                                 return_dict=True,
-                                                 max_seq_len=8192,
-                                                 trust_remote_code=True)
-#langchain_model = AutoModel.from_pretrained(args.model_path)
-langchain_pipe = pipeline("text-generation", model=langchain_model,
-                          tokenizer=langchain_tokenizer, max_new_tokens=256)
+from mpt_wrapper import MosaicML
+import torch
+import intel_extension_for_pytorch as ipex
+config = AutoConfig.from_pretrained("./mpt-7b-chat", trust_remote_code=True)
+config.attn_config['attn_impl'] = "torch"
+config.init_device = 'cuda:0' if torch.cuda.is_available() else "cpu"
+langchain_model = AutoModelForCausalLM.from_pretrained(
+    "./mpt-7b-chat",
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    config=config,
+)
+langchain_tok = AutoTokenizer.from_pretrained("./mpt-7b-chat", trust_remote_code=True)
+langchain_model = ipex.optimize(langchain_model, dtype=torch.bfloat16)
+stop_token_ids = langchain_tok.convert_tokens_to_ids(["<|im_end|>", "<|endoftext|>"])
+langchain_tok.pad_token = langchain_tok.eos_token
+langchain_tok.add_special_tokens({'pad_token': '[PAD]'})
+if not os.path.exists("/tmp/young_pat_dense_retrieval"):
+    from young_pat import d_load_xlsx, persist_embedding
+    documents = d_load_xlsx("./doc/young_pat/pat.xlsx", True)
+    persist_embedding(documents, "/tmp/young_pat_dense_retrieval", model_path="hkunlp/instructor-large")
+langchain_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
+langchain_vectordb = Chroma(persist_directory="/tmp/young_pat_dense_retrieval", embedding_function=langchain_embeddings)
+langchain_retriever = langchain_vectordb.as_retriever(search_type = "mmr", search_kwargs = {"k": 1, "fetch_k": 5})
+
+class StopOnTokens(StoppingCriteria):
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        for stop_id in stop_token_ids:
+            if input_ids[0][-1] == stop_id:
+                return True
+        return False
 
 from haystack.nodes.prompt.prompt_node import PromptNode
 prompt = PromptNode(model_name_or_path = pmodel)
@@ -157,8 +180,6 @@ wiki_pipeline.add_node(component=prompt, name="Prompter", inputs=["Shaper"])
 #chatLLM.load_model(model_name_or_path="./chatglm-6b")
 #chatLLM.temperature = 0.05
 #chatLLM.top_p = 0.9
-
-
 
 app = FastAPI()
 router = APIRouter()
@@ -200,7 +221,7 @@ class QueryResponse(BaseModel):
     timings: Optional[Dict] = None
     results: Optional[List] = None
 
-@router.post("/fastrag/query")
+@router.post("/v1/chat/completions")
 def query(request: QueryRequest):
     domain = request.domain
     logger.info(f"fastrag query received for {domain}")
@@ -217,21 +238,23 @@ def query(request: QueryRequest):
                 output_path = '/tmp/ask_gm_dense_retrieval_chinese'
                 embeddings = HuggingFaceInstructEmbeddings(model_name="shibing624/text2vec-base-chinese")
             vectordb = Chroma(persist_directory=output_path, embedding_function=embeddings)
-            retriever = vectordb.as_retriever(search_type = "mmr", search_kwargs = {"k": 1, "fetch_k": 5})
+            retriever = vectordb.as_retriever(search_type = "mmr", search_kwargs = {"k": 2, "fetch_k": 5})
             prompt_template = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{question}\n\n### Input:\n{context}\n\n### Response:"""
             from langchain.prompts import PromptTemplate
             PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-            docs = retriever.get_relevant_documents(request.query)
-            logger.info("dense retreival done...")
-            hf = HuggingFacePipeline(pipeline=langchain_pipe)
-            chain = load_qa_chain(hf, chain_type="stuff", prompt=PROMPT)
-            chain_result = chain({"input_documents": docs, "question": query}, return_only_outputs=True)
+            chain_type_kwargs = {"prompt": PROMPT}
+            start_time = time.time()
+            qa = RetrievalQA.from_chain_type(llm=langchain_model, retriever=retriever, chain_type="stuff",
+                                     chain_type_kwargs=chain_type_kwargs)
+            chain_result = qa.run(request.query)
+            end_time = time.time()
+            print("inference cost {} seconds.".format(end_time - start_time))
             print(chain_result)
-            result = _process_dense_retrieval(chain_result)
+            result = _process_dense_retrieval(request, chain_result)
         elif request.embedding == 'sparse':
-            document_store = ElasticsearchDocumentStore(host="localhost", index="elastic_askgm_sparse", port=9200, search_fields=["content", "title"])
+            askgm_document_store = ElasticsearchDocumentStore(host="localhost", index="elastic_askgm_sparse", port=9200, search_fields=["content", "title"])
             askgm_pipeline = Pipeline()
-            askgm_retriever = BM25Retriever(document_store=document_store, top_k = 10)
+            askgm_retriever = BM25Retriever(document_store=askgm_document_store, top_k = 10)
             askgm_pipeline.add_node(component=askgm_retriever, name="Retriever", inputs=["Query"])
             askgm_pipeline.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
             askgm_pipeline.add_node(component=shaper, name="Shaper", inputs=["Reranker"])
@@ -244,8 +267,66 @@ def query(request: QueryRequest):
             prompt.add_prompt_template(custom_template)
             prompt.default_prompt_template = custom_template
             askgm_pipeline.add_node(component=prompt, name="Prompter", inputs=["Shaper"])
-            logger.info("sparse retrieval done...")
+            logger.info("Sparse Retrieval for ASK_GM done...")
             result = _process_request(askgm_pipeline, request)
+        elif domain == "Young_Pat":
+            if request.embedding == "dense":
+                docs = langchain_retriever.get_relevant_documents(request.query)
+                documents=[]
+                for doc in docs:
+                    print(doc.page_content)
+                    documents.append(doc.page_content)
+                context = " ".join(documents)
+                prompt= """Have a conversation with a human, answer the following questions as best you can.
+                        You can refer to the following document and context.\n Question:\n{}\n Context:\n{}\n Response:""".format(request.query, context)
+                stop = StopOnTokens()
+                inputs = langchain_tok(prompt, return_tensors="pt", padding=True)
+                inputs = inputs.to(langchain_model.device)
+                generate_kwargs = dict(
+                    use_cache=True,
+                    bos_token_id=0,
+                    eos_token_id=1,
+                    pad_token_id=0,
+                    min_new_tokens=1,
+                    max_new_tokens=256,
+                    temperature=0.001,
+                    top_p=0.9,
+                    top_k=3,
+                    repetition_penalty=1.1,
+                    do_sample=True,
+                    stopping_criteria=StoppingCriteriaList([stop])
+                )
+                generation_config = GenerationConfig(**generate_kwargs)
+                start_time = time.time()
+                with torch.no_grad():
+                    with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16, cache_enabled=True):
+                        output = langchain_model.generate(**inputs, generation_config=generation_config)
+                    output = output[0][inputs["input_ids"].shape[-1]:]
+                generated_texts = langchain_tok.decode(output, skip_special_tokens=True)
+                end_time = time.time()
+                print("inference cost {} seconds.".format(end_time - start_time))
+                print(generated_texts)
+                result = _process_dense_retrieval(generated_texts)
+            else:
+                customized_document_store = InMemoryDocumentStore(use_gpu=False, use_bm25=True)
+                from young_pat import s_load_xlsx
+                customized_document_store = s_load_xlsx("./doc/young_pat/pat.xlsx", True, customized_document_store)
+                young_pat_pipeline = Pipeline()
+                young_pat_retriever = BM25Retriever(document_store=customized_document_store, top_k = 10)
+                young_pat_pipeline.add_node(component=young_pat_retriever, name="Retriever", inputs=["Query"])
+                young_pat_pipeline.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
+                young_pat_pipeline.add_node(component=shaper, name="Shaper", inputs=["Reranker"])
+                from haystack.nodes.prompt.prompt_node import PromptNode
+                from haystack.nodes.prompt.prompt_node import PromptTemplate
+                prompt = PromptNode(model_name_or_path = pmodel)
+                custom_template = PromptTemplate(
+                            name="llama-instruct",
+                            prompt_text="Given the context please answer the question. Context: $documents; Question: $query; Answer:")
+                prompt.add_prompt_template(custom_template)
+                prompt.default_prompt_template = custom_template
+                young_pat_pipeline.add_node(component=prompt, name="Prompter", inputs=["Shaper"])
+                logger.info("Sparse Retrieval for Young_Pat done...")
+                result = _process_request(young_pat_pipeline, request)
     elif domain == "Customized":
         if request.blob:
             file_content = base64.b64decode(request.blob)
@@ -307,13 +388,32 @@ def query(request: QueryRequest):
         result = _process_request(customized_pipeline, request)
     return result
 
+@router.post("/v1/retrieval/llmcache")
+async def get_cache(request: QueryRequest):
+    prompt = request.query
+    from ..llmcache.cache import get
+    result = get(prompt)
+    print(result)
+    if(result == None):
+        print("cache miss >>>>>>>>>>>>>>>")
+        response = RedirectResponse(url="/v1/chat/completions")
+        return response
+    else:
+        print("cache hit >>>>>>>>>>>>>>>>")
+        def stream_results():
+            yield "data: Response from Cache: {}\n\n".format(result['choices'][0]['text'])
+            yield "data: [DONE]\n\n"
+
+        return StreamingResponse(stream_results(), media_type="text/event-stream")
+
 app.include_router(router)
 
 @send_event_if_public_demo
-def _process_dense_retrieval(result):
+def _process_dense_retrieval(request, result):
     text = result['output_text'].replace('\n', '')
     #text = result['result'].replace('\n', '')
-    print("=================", text)
+    from ..llmcache.cache import put
+    put(request.query, text)
     def stream_results():
         yield "data: {}\n\n".format(text)
         yield "data: [DONE]\n\n"
@@ -343,8 +443,7 @@ def _process_request(pipeline, request) -> Dict[str, Any]:
         prediction["answers"] = []
 
     def stream_results():
-        for idx, result in enumerate(prediction["results"]):
-            #data = json.dumps({"text": result, "error_code": 0}).encode() + b"\0"
+        for _, result in enumerate(prediction["results"]):
             yield f"data: {result}\n\n"
         yield f"data: [DONE]\n\n"
 
@@ -358,7 +457,8 @@ def _process_request(pipeline, request) -> Dict[str, Any]:
             default=str,
         )
     )
-
+    from ..llmcache.cache import put
+    put(request.query, prediction["results"])
     return StreamingResponse(stream_results(), media_type="text/event-stream")
 
 def _format_filters(filters):
@@ -393,5 +493,11 @@ def _format_filters(filters):
 
 
 if __name__ == "__main__":
+    from ..llmcache.cache import init_similar_cache_from_config, put
+    if args.cache_chat_config_file:
+        init_similar_cache_from_config(config_dir=args.cache_chat_config_file,
+                                       embedding_model_dir=args.cache_embedding_model_dir)
+        put("test","test")
+
     uvicorn.run(app, host=args.host, port=args.port, log_level="info")
 
