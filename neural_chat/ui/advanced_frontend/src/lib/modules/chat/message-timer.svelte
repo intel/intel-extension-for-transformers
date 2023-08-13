cddcef596fb2742e4b40bc46ae9f6879d803649f
Lv, Liang1 <liang1.lv@intel.com>
2023-08-13 21:47:34 +0800

    add frontend code
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>

---
+++ neural_chat/ui/advanced_frontend/src/lib/modules/chat/message-timer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import timeDifference from '$lib/common/timediff';
+
+    const messageTime = new Date()
+    let messageTimer = 'Just now'
+
+    setInterval(function() {
+        var now = new Date(),
+        current = now.getTime();
+    
+        messageTimer = timeDifference(current, messageTime.getTime())
+    }, 5000);
+</script>
+
+<span class="text-xs text-gray-500 leading-none">{messageTimer}</span>
---
+++ neural_chat/ui/advanced_frontend/src/routes/chat/[chatId]/+page.ts
@@ -0,0 +1,7 @@
+export const load = async ({ params }) => {
+  const chatId = params.chatId;
+
+  return {
+    chatId
+  };
+};
