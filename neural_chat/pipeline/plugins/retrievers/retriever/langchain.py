7c6f00400110c333abd8a90ff26b171be1297b1b
XuhuiRen <44249229+XuhuiRen@users.noreply.github.com>
2023-08-17 23:53:53 +0800

    Update langchain.py
    fix small typo

--- neural_chat/pipeline/plugins/retrievers/retriever/langchain.py
+++ neural_chat/pipeline/plugins/retrievers/retriever/langchain.py
@@ -4,4 +4,4 @@ from langchain.vectorstores import Chroma
 
 class ChromaRetriever():
     def __init__(self, database, search_type="mmr", search_kwargs={"k": 1, "fetch_k": 5}):
-        self.retriever = self.database.as_retriever(search_type=search_type, search_kwargs=search_kwargs)
+        self.retriever = database.as_retriever(search_type=search_type, search_kwargs=search_kwargs)
