6200abdbfa0672433f0f0a530935744dcbbe501a
XuhuiRen <xuhui.ren@intel.com>
2023-08-17 10:54:23 +0800

    revision
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>

--- neural_chat/pipeline/plugins/retrievers/retriever/__init__.py
+++ neural_chat/pipeline/plugins/retrievers/retriever/__init__.py
@@ -1,7 +1,7 @@
-"""Function to check the intent of the input user query with LLM."""
+"""The class defination for the retriever. Supporting langchain-based and haystack-based retriever."""
 
 from langchain.retrievers.elastic_search_bm25 import ElasticSearchBM25Retriever
-from .langchain_retriever import ChromaRetriever
+from .langchain import ChromaRetriever
 
 from fastrag.retrievers import BaseRetriever, ColBERTRetriever
 from haystack.nodes import BM25Retriever
