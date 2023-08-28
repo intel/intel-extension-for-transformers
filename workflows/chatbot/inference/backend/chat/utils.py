4d7fc0e0b8897fae8d5b48a8bef80e26be34dc89
Liangyx2 <106130696+Liangyx2@users.noreply.github.com>
2023-08-28 10:40:06 +0800

    Update utils.py

--- workflows/chatbot/inference/backend/chat/utils.py
+++ workflows/chatbot/inference/backend/chat/utils.py
@@ -6,7 +6,7 @@ import sys
 
 import requests
 
-from constants import LOGDIR
+from .constants import LOGDIR
 
 server_error_msg = "**NETWORK ERROR DUE TO HIGH TRAFFIC. PLEASE REGENERATE OR REFRESH THIS PAGE.**"
 moderation_msg = "YOUR INPUT VIOLATES OUR CONTENT MODERATION GUIDELINES. PLEASE TRY AGAIN."
