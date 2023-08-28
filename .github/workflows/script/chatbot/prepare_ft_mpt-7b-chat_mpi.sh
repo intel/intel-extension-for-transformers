e79bf0f43f4f36d3cdd29a1d8d48e38aa08b1046
jiafu zhang <jiafu.zhang@intel.com>
2023-08-28 05:44:32 +0000

    add finetuning test for mosaicml-mpt-7b-chat with mpi
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
+++ .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
@@ -8,7 +8,11 @@ export MASTER_ADDR=$master_node
 export I_MPI_HYDRA_IFACE=eth0
 EOF
 )"
+# for launching mpirun from yaml
+docker exec "chatbotfinetune-mpi-s0" bash -c "cd /root/chatbot; echo \"source activate && conda activate chatbot-finetuning\" > bash_setup.sh; echo \"$prepare_script\" >> bash_setup.sh"
+# for ssh setup mpi and oneccl properly
 docker exec "chatbotfinetune-mpi-s0" bash -c "echo \"$prepare_script\" >> ~/.bashrc"
 docker exec "chatbotfinetune-mpi-s1" bash -c "echo \"$prepare_script\" >> ~/.bashrc"
+
 echo "$master_node" > ./hosts2
 echo "$slave_node" >> ./hosts2
