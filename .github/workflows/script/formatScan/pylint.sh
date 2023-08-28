a002cdd4bf42548bf111b7b08b7db38da85b640b
Wenxin Zhang <wenxin.zhang@intel.com>
2023-08-28 21:49:01 +0800

    fix pylint

--- .github/workflows/script/formatScan/pylint.sh
+++ .github/workflows/script/formatScan/pylint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 source /intel-extension-for-transformers/.github/workflows/script/change_color.sh
+cd /intel-extension-for-transformers
+$BOLD_YELLOW && echo "---------------- git submodule update --init --recursive -------------" && $RESET
+git config --global --add safe.directory "*"
+git submodule update --init --recursive
+
+$BOLD_YELLOW && echo "---------------- run python setup.py sdist bdist_wheel -------------" && $RESET
+pip install build --upgrade
+python3 -m build -s -w
+$BOLD_YELLOW && echo "---------------- pip install binary -------------" && $RESET
+pip install dist/intel_extension_for_transformers*.whl
+pip list
 
 cd /intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/
 if [ -f "requirements.txt" ]; then
@@ -22,12 +33,16 @@ fi
 pip install accelerate intel_extension_for_pytorch nlpaug nltk
 pip install git+https://github.com/EleutherAI/lm-evaluation-harness.git@83dbfbf6070324f3e5872f63e49d49ff7ef4c9b3
 
+echo "[DEBUG] list pipdeptree..."
+pip install pipdeptree
+pipdeptree
+
 python -m pylint -f json --disable=R,C,W,E1129 \
     --enable=line-too-long \
     --max-line-length=120 \
     --extension-pkg-whitelist=numpy,nltk \
     --ignored-classes=TensorProto,NodeProto \
-    --ignored-modules=tensorflow,torch,torch.quantization,torch.tensor,torchvision,mxnet,onnx,onnxruntime,neural_compressor,engine_py,neural_engine_py,intel_extension_for_transformers.neural_engine_py,neural_compressor.benchmark,intel_extension_for_transformers.lm_eval*,ntel_extension_for_transformers.evaluation.hf_eval* \
+    --ignored-modules=tensorflow,torch,torch.quantization,torch.tensor,torchvision,mxnet,onnx,onnxruntime,neural_compressor,engine_py,neural_engine_py,intel_extension_for_transformers.neural_engine_py,neural_compressor.benchmark \
     /intel-extension-for-transformers/intel_extension_for_transformers >${log_dir}/pylint.json
 exit_code=$?
 
