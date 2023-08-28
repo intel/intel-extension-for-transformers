165cf1be754174ba8a2f6412747ba3b88f331568
ZheWang <zhe1.wang@intel.com>
2023-08-28 06:31:28 -0700

    add copyright in operator dir

--- intel_extension_for_transformers/llm/operator/cscr/dispatcher/jblas_task_dispatcher.hpp
+++ intel_extension_for_transformers/llm/operator/cscr/dispatcher/jblas_task_dispatcher.hpp
@@ -1,6 +1,21 @@
+//  Copyright (c) 2023 Intel Corporation
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 #pragma once
 
 #include <torch/torch.h>
+
 #include "jblas/jit_blas_weight_compression.h"
 
 enum QBITS_TASK {
@@ -30,4 +45,5 @@ struct qbits_runtime_ctx {
   jblas::prologue::PackedWeight* deseries_wei;
 };
 
-void task_dispatcher(qbits_config_param* p, qbits_runtime_ctx* ctx, QBITS_TASK task);
+void task_dispatcher(qbits_config_param* p, qbits_runtime_ctx* ctx,
+                     QBITS_TASK task);
