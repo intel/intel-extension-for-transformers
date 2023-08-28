d45fe6f656bb83462b684e5c79c3786157f86eee
Liangyx2 <106130696+Liangyx2@users.noreply.github.com>
2023-08-28 10:37:57 +0800

    Update run_ipex.sh

--- workflows/chatbot/inference/backend/chat/run_ipex.sh
+++ workflows/chatbot/inference/backend/chat/run_ipex.sh
@@ -2,7 +2,8 @@
 ps -ef |grep 'controller' |awk '{print $2}' |xargs kill -9
 ps -ef |grep 'model_worker' |awk '{print $2}' |xargs kill -9
 
-python -m controller &
+python -m backend.chat.controller --port 8000 &  --cache-chat-config-file=backend/llmcache/cache_config.yml --cache-embedding-model-dir=backend/llmcache/instructor-large
+sleep 10
 
 # KMP
 export KMP_BLOCKTIME=1
@@ -10,12 +11,11 @@ export KMP_SETTINGS=1
 export KMP_AFFINITY=granularity=fine,compact,1,0
 
 # OMP
-export OMP_NUM_THREADS=32
+export OMP_NUM_THREADS=56
 export LD_PRELOAD=${CONDA_PREFIX}/lib/libiomp5.so
 
 # tc malloc
 export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libtcmalloc.so
 
 # ipex
-# numactl -l -C 0-31 python -m model_worker --ipex --model-path ./llama-7b-hf-conv --controller-address http://localhost:80 --worker-address http://localhost:8080 --device "cpu"
-numactl -l -C 0-31 python -m model_worker --ipex --model-path ./mpt-7b-chat --controller-address http://localhost:80 --worker-address http://localhost:8080 --device "cpu"
+numactl -l -C 0-55 python -m backend.chat.model_worker --ipex --model-path ./backend/chat/mpt-7b-chat --controller-address http://localhost:8000 --worker-address http://localhost:8080 --device cpu
