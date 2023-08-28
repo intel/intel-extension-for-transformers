755405e7e4743f60a1b8cbe6f2d410ab6251de62
jiafu zhang <jiafu.zhang@intel.com>
2023-08-28 07:20:41 +0000

    add finetuning test for mosaicml-mpt-7b-chat with mpi
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
+++ .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
@@ -7,13 +7,11 @@ export CCL_WORKER_COUNT=1
 export I_MPI_HYDRA_IFACE=eth0
 EOF
 )"
-# resolve master_node first
-prepare_script=$(echo -e "$prepare_script\nexport MASTER_ADDR=$master_node")
 # for launching mpirun from yaml
-docker exec "chatbotfinetune-mpi-s0" bash -c "cd /root/chatbot; echo \"source activate && conda activate chatbot-finetuning\" > bash_setup.sh; echo \"$prepare_script\" >> bash_setup.sh"
+docker exec "chatbotfinetune-mpi-s0" bash -c "cd /root/chatbot; echo \"source activate && conda activate chatbot-finetuning\" > bash_setup.sh; echo \"$prepare_script\" >> bash_setup.sh; echo export MASTER_ADDR=$master_node >> bash_setup.sh"
 # for ssh setup mpi and oneccl properly
-docker exec "chatbotfinetune-mpi-s0" bash -c "echo \"$prepare_script\" >> ~/.bashrc"
-docker exec "chatbotfinetune-mpi-s1" bash -c "echo \"$prepare_script\" >> ~/.bashrc"
+docker exec "chatbotfinetune-mpi-s0" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
+docker exec "chatbotfinetune-mpi-s1" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
 
 echo "$master_node" > ./hosts2
 echo "$slave_node" >> ./hosts2
