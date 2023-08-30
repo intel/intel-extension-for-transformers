350d089433f3e031e294e1e5279d6c36eb942cc1
Wang, Wenqi2 <wenqi2.wang@intel.com>
2023-08-30 16:04:53 +0800

    rebase

--- workflows/chatbot/inference/backend/fastrag/fastrag_service.py
+++ workflows/chatbot/inference/backend/fastrag/fastrag_service.py
@@ -457,7 +457,8 @@ def query(request: QueryRequest):
         if request.blob:
             file_content = base64.b64decode(request.blob)
             random_suffix = str(uuid.uuid4().hex)
-            file_path = f"/tmp/customized_doc_{random_suffix}" + request.filename
+			sanitized_filename = os.path.basename(request.filename)
+			file_path = f"/tmp/customized_doc_{random_suffix}_{sanitized_filename}"
             with open(file_path, "wb") as f:
                 f.write(file_content)
 
