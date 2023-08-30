0ac7989ccc7f7c5107302197843b91feb1a403e7
Wenxin Zhang <wenxin.zhang@intel.com>
2023-08-30 16:17:52 +0800

    engine UT parallal
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- .github/workflows/script/SparseLibCI/run_sparse_lib.sh
+++ .github/workflows/script/SparseLibCI/run_sparse_lib.sh
@@ -2,6 +2,7 @@
 export COVERAGE_RCFILE="/intel-extension-for-transformers/.github/workflows/script/unitTest/coverage/.coveragerc"
 
 output_log_dir="/intel-extension-for-transformers/benchmark_log"
+WORKING_DIR="/intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime"
 mkdir ${output_log_dir}
 mkdir ${output_log_dir}/cur
 cur_dir=${output_log_dir}/cur
@@ -10,27 +11,27 @@ cp /intel-extension-for-transformers/.github/workflows/script/SparseLibCI/genera
 cp /intel-extension-for-transformers/.github/workflows/script/SparseLibCI/generate_sparse_lib.py /generate_sparse_lib.py
 pip install cmake
 
-rm -rf /intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime/build
+rm -rf ${WORKING_DIR}/build
 cd /intel-extension-for-transformers
 
 git config --global --add safe.directory "*"
 git fetch
 git submodule update --init --recursive
-cd /intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime
+cd ${WORKING_DIR}
 mkdir build && cd build
 
 CC=gcc CXX=g++ cmake .. -DNE_WITH_SPARSELIB=ON -DNE_WITH_TESTS=ON -DNE_WITH_SPARSELIB_BENCHMARK=ON -DPYTHON_EXECUTABLE=$(which python)
 make -j
 cd bin
-bash /intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime/build/bin/ci/run_ci.sh $cur_dir
+bash ${WORKING_DIR}/build/bin/ci/run_ci.sh $cur_dir
 
 for caselog in $(find $cur_dir/*); do
     case_name=$(echo $caselog | sed -e 's/\.log$//')
     $BOLD_YELLOW && echo "[VAL INFO] write summary, case_name=$case_name" && $RESET
-    bash /intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime/test/kernels/benchmark/ci/to_summary.sh $caselog | tee -a "${case_name}_summary.log"
+    bash ${WORKING_DIR}/test/kernels/benchmark/ci/to_summary.sh $caselog | tee -a "${case_name}_summary.log"
 done
 
-cd /intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime
+cd ${WORKING_DIR}
 mkdir refer && cd refer
 git checkout -b refer origin/main
 git pull
@@ -39,10 +40,10 @@ make -j
 cd bin
 
 mkdir -p ${output_log_dir}/ref
-bash /intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime/refer/bin/ci/run_ci.sh $ref_dir
+bash ${WORKING_DIR}/refer/bin/ci/run_ci.sh $ref_dir
 
 for caselog in $(find $ref_dir/*); do
     case_name=$(echo $caselog | sed -e 's/\.log$//')
     $BOLD_YELLOW && echo "[VAL INFO] write summary, case_name=$case_name" && $RESET
-    bash /intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime/test/kernels/benchmark/ci/to_summary.sh $caselog | tee -a "${case_name}_summary.log"
+    bash ${WORKING_DIR}/test/kernels/benchmark/ci/to_summary.sh $caselog | tee -a "${case_name}_summary.log"
 done
