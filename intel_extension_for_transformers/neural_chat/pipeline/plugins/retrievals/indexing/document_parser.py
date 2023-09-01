eb300607cb5ccd9fb3910904793e383a68576b71
XuhuiRen <xuhui.ren@intel.com>
2023-09-01 21:59:34 +0800

    revise
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>

--- intel_extension_for_transformers/neural_chat/pipeline/plugins/retrievals/indexing/document_parser.py
+++ intel_extension_for_transformers/neural_chat/pipeline/plugins/retrievals/indexing/document_parser.py
@@ -29,7 +29,7 @@ from .utils import load_unstructured_data, laod_structured_data, get_chuck_data
 
 class DocumentIndexing:
     def __init__(self, retrieval_type="dense", document_store=None, persist_dir="./output",
-                 process=False, embedding_model="/data1/lkk/instructor_large/", max_length=512):
+                 process=False, embedding_model="hkunlp/instructor-large", max_length=512):
         """
         Wrapper for document indexing. Support dense and sparse indexing method.
         """
