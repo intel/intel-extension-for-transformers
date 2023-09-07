2651bd85f4048656117bde69b2146d9a317e6614
lvliang-intel <liang1.lv@intel.com>
2023-09-08 07:34:31 +0800

    Fix retrieval example issue (#258)
    * Fix retrieval example issue
    
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>
    
    * update readme
    
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>
    
    * fix copyright
    
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>
    
    * Fix the readme and script
    
    Signed-off-by: hshen14 <haihao.shen@intel.com>
    
    * Refine main readme
    
    Signed-off-by: hshen14 <haihao.shen@intel.com>
    
    * fix name
    
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>
    
    * Refine main readme
    
    Signed-off-by: hshen14 <haihao.shen@intel.com>
    
    * Refine main readme
    
    Signed-off-by: hshen14 <haihao.shen@intel.com>
    
    * add xlsx
    
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>
    
    * Refine main readme
    
    Signed-off-by: hshen14 <haihao.shen@intel.com>
    
    * Refine main readme
    
    Signed-off-by: hshen14 <haihao.shen@intel.com>
    
    * fix pylint issues
    
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>
    
    * update dict
    
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>
    
    ---------
    
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>
    Signed-off-by: hshen14 <haihao.shen@intel.com>
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>
    Co-authored-by: hshen14 <haihao.shen@intel.com>
    Co-authored-by: XuhuiRen <xuhui.ren@intel.com>
    Co-authored-by: XuhuiRen <44249229+XuhuiRen@users.noreply.github.com>
    Co-authored-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- intel_extension_for_transformers/neural_chat/examples/retrieval/retrieval_chat.py
+++ intel_extension_for_transformers/neural_chat/examples/retrieval/retrieval_chat.py
@@ -15,34 +15,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import sys
-from transformers import TrainingArguments, HfArgumentParser
 from intel_extension_for_transformers.neural_chat.config import PipelineConfig
 from intel_extension_for_transformers.neural_chat.chatbot import build_chatbot
 from intel_extension_for_transformers.neural_chat.plugins import plugins
 
-
 def main():
-    # See all possible arguments in config.py
-    # or by passing the --help flag to this script.
-    # We now keep distinct sets of args, for a cleaner separation of concerns.
-    parser = HfArgumentParser(PipelineConfig)
-    
-    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
-        # If we pass only one argument to the script and it's the path to a json file,
-        # let's parse it to get our arguments.
-        pipeline_args= parser.parse_json_file(json_file = os.path.abspath(sys.argv[1]))
-    else:
-        pipeline_args= parser.parse_args_into_dataclasses()
-
     plugins.retrieval.enable = True
-    plugins.retrieval.args["retrieval_input_path"] = "./Annual_report.pdf"
-    pipeline_args.plugins = plugins
+    plugins.retrieval.args["input_path"] = "./Annual_Report.pdf"
+    pipeline_args = PipelineConfig(plugins=plugins)
     chatbot = build_chatbot(pipeline_args)
 
-    response = chatbot.predict(query="What is IDM 2.0?", config=pipeline_args)
-    print(response.choices[0]["text"])
+    response = chatbot.predict(query="What is IDM 2.0?")
+    print(response)
 
 if __name__ == "__main__":
     main()
