87e99ac95385a5475aead84d025f4313383e80db
Hengyu Meng <hengyu.meng@intel.com>
2023-09-07 20:36:08 -0700

    fix format

--- intel_extension_for_transformers/llm/runtime/graph/models/falcon/falcon.cpp
+++ intel_extension_for_transformers/llm/runtime/graph/models/falcon/falcon.cpp
@@ -37,7 +37,6 @@
 #include "models/model_utils/model_utils.h"
 #include "models/model_utils/util.h"
 
-
 // evaluate the transformer
 //
 //   - lctx:      model context
@@ -210,14 +209,14 @@ static bool falcon_model_eval_internal(model_context& lctx, const model_token* t
     {
       // FFN FUSION
       if (jblas_fusion_FFN_GeLu_f32f32_support(model.layers[il].ffn[0]->data, model.layers[il].ffn[1]->data,
-                                                 N * batch_size, cur->ne[0], model.layers[il].ffn[0]->ne[1],
-                                                 model.layers[il].ffn[1]->ne[1])) {
-      cur = ne_ffn_gelu(ctx0, model.layers[il].ffn[0], model.layers[il].ffn[1],  inpFF);
-    } else {
-      cur = ne_mul_mat(ctx0, model.layers[il].ffn[0], inpFF);
-      cur = ne_gelu(ctx0, cur);
-      cur = ne_mul_mat(ctx0, model.layers[il].ffn[1], cur);
-    }
+                                               N * batch_size, cur->ne[0], model.layers[il].ffn[0]->ne[1],
+                                               model.layers[il].ffn[1]->ne[1])) {
+        cur = ne_ffn_gelu(ctx0, model.layers[il].ffn[0], model.layers[il].ffn[1], inpFF);
+      } else {
+        cur = ne_mul_mat(ctx0, model.layers[il].ffn[0], inpFF);
+        cur = ne_gelu(ctx0, cur);
+        cur = ne_mul_mat(ctx0, model.layers[il].ffn[1], cur);
+      }
     }
 
     cur = ne_add(ctx0, cur, attn_out);
