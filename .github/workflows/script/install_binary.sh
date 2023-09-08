e46a9a07ea3875f601e1c563245e2cb0953fd418
Wenxin Zhang <wenxin.zhang@intel.com>
2023-09-08 20:36:00 +0800

    fix install binary

--- .github/workflows/script/install_binary.sh
+++ .github/workflows/script/install_binary.sh
@@ -7,7 +7,7 @@ git config --global --add safe.directory "*"
 git submodule update --init --recursive
 
 $BOLD_YELLOW && echo "---------------- run python setup.py sdist bdist_wheel -------------" && $RESET
-python setup.py bdist_wheel
+python setup.py sdist bdist_wheel
 
 $BOLD_YELLOW && echo "---------------- pip install binary -------------" && $RESET
 pip install dist/intel_extension_for_transformers*.whl
