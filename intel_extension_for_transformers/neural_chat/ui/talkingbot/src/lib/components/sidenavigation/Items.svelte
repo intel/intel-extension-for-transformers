cc1f222cbcd0ff40cd389dc73d8066f4a77b626c
WenjiaoYue <108783334+WenjiaoYue@users.noreply.github.com>
2023-09-11 07:04:59 +0800

    upload talking bot front_end code (#270)
    * upload talking bot front_end code
    
    * move talkingbot to ui folder, update readme
    
    Signed-off-by: WenjiaoYue <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>
    
    * Modified the readme according to the suggestion
    
    * Replace images to avoid copyright issues
    
    Signed-off-by: WenjiaoYue <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>
    
    * exchange pic name
    
    Signed-off-by: WenjiaoYue <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>
    
    ---------
    
    Signed-off-by: WenjiaoYue <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>
    Co-authored-by: WenjiaoYue <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>

---
+++ intel_extension_for_transformers/neural_chat/ui/talkingbot/src/lib/components/sidenavigation/Items.svelte
@@ -0,0 +1,31 @@
+<script>
+	import data from './data';
+	import { page } from '$app/stores';
+	import { open } from '$lib/components/shared/store';
+
+	const style = {
+		title: `mx-4 text-sm`,
+		inactive: `text-white hover:text-blue-200`,
+		active: `font-medium text-blue-300 hover:text-blue-300`,
+		link: `flex items-center justify-start my-2 p-4 w-full`,
+		close: `lg:duration-700 lg:ease-out lg:invisible lg:opacity-0 lg:transition-all`,
+		open: `lg:duration-500 lg:ease-in lg:h-auto lg:opacity-100 lg:transition-all lg:w-auto`
+	};
+</script>
+
+<ul class="md:pl-5">
+	<li>
+		{#each data as item, i (item.title)}
+			<a
+				href={item.link}
+				class={`${style.link} 
+            ${item.link === $page.url.pathname ? style.active : style.inactive}`}
+			>
+				<span><svelte:component this={item.icon} /></span>
+				<span class={`${style.title} ${$open ? style.open : style.close}`}>
+					{item.title}
+				</span>
+			</a>
+		{/each}
+	</li>
+</ul>
