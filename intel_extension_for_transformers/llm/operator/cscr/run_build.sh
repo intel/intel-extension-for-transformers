84de01637b70c633a4c4fdce16560c21247e05e0
Wang,Zhe <zhe1.wang@intel.com>
2023-09-12 10:04:26 +0800

    adapt with latest jblas.
    Signed-off-by: Wang,Zhe <zhe1.wang@intel.com>

--- intel_extension_for_transformers/llm/operator/cscr/run_build.sh
+++ intel_extension_for_transformers/llm/operator/cscr/run_build.sh
@@ -11,10 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-rm -rf build
-mkdir build
+rm -rf build;
+mkdir build;
 cd build
 
-cmake ..
+cmake  ..
 
 make -j$(nproc)
