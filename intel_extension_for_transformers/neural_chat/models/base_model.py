710953c376a8484c3ba093950436784f0267e94b
XuhuiRen <xuhui.ren@intel.com>
2023-09-15 12:00:44 +0800

    add
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>

--- intel_extension_for_transformers/neural_chat/models/base_model.py
+++ intel_extension_for_transformers/neural_chat/models/base_model.py
@@ -189,7 +189,7 @@ class BaseModel(ABC):
                         if plugin_name == "safety_checker" and response:
                             return "Your query contains sensitive words, please try another query."
                         else:
-                            if response != None:
+                            if response != None and response != False:
                                 query = response
         assert query is not None, "Query cannot be None."
 
