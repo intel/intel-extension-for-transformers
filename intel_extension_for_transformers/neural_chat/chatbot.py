29d1d6f88580e1ef17f65a9b2992a3e5b2733917
Lv, Liang1 <liang1.lv@intel.com>
2023-09-15 17:40:33 +0800

    Fix habana docker dependency issue
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>

--- intel_extension_for_transformers/neural_chat/chatbot.py
+++ intel_extension_for_transformers/neural_chat/chatbot.py
@@ -21,7 +21,7 @@ from intel_extension_for_transformers.llm.quantization.optimization import Optim
 from .config import PipelineConfig
 from .config import BaseFinetuningConfig
 from .config import DeviceOptions
-from .plugins import plugins, global_plugins
+from .plugins import plugins
 
 def build_chatbot(config: PipelineConfig=None):
     """Build the chatbot with a given configuration.
@@ -101,8 +101,13 @@ def build_chatbot(config: PipelineConfig=None):
                 plugins[plugin_name]["instance"] = plugins[plugin_name]['class'](**plugin_value['args'])
                 adapter.register_plugin_instance(plugin_name, plugins[plugin_name]["instance"])
 
-    global_plugins.reset_plugins()
-    plugins = global_plugins.plugins
+    for key in plugins:
+        plugins[key] = {
+            "enable": False,
+            "class": None,
+            "args": {},
+            "instance": None
+        }
 
     parameters = {}
     parameters["model_name"] = config.model_name_or_path
