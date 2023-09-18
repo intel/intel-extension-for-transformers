b7eb1e73290bc30490d5ae971541beae0db0d056
XuhuiRen <44249229+XuhuiRen@users.noreply.github.com>
2023-09-18 14:44:59 +0800

    Update test_inference.py

--- intel_extension_for_transformers/neural_chat/tests/api/test_inference.py
+++ intel_extension_for_transformers/neural_chat/tests/api/test_inference.py
@@ -55,6 +55,7 @@ class UnitTest(unittest.TestCase):
                                 plugins=plugins)
         chatbot = build_chatbot(config)
         response = chatbot.predict("How many cores does the Intel® Xeon® Platinum 8480+ Processor have in total?")
+        print(response)
         self.assertTrue("56" in response)
 
     def test_voice_chat(self):
