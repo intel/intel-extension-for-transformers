435ea48a00d940e1dadbd2514f46c5ff65617b51
Lv, Kaokao <kaokao.lv@intel.com>
2023-09-25 22:37:55 +0800

    update dpo trainer.

--- intel_extension_for_transformers/transformers/dpo_trainer.py
+++ intel_extension_for_transformers/transformers/dpo_trainer.py
@@ -1,6 +1,3 @@
-# DPO Authors: Rafael Rafailov, Archit Sharma, Eric Mitchell, Stefano Ermon, Christopher D. Manning, and Chelsea Finn 2023
-# Copyright 2023 The HuggingFace Team. All rights reserved.
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,6 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import warnings
 from collections import defaultdict
 from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Union
