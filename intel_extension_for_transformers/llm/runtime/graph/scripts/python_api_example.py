cc5701ce7f841913f09017ce290217d98b67c461
zhenwei-intel <zhenwei.liu@intel.com>
2023-09-25 15:35:40 +0800

    update
    Signed-off-by: zhenwei-intel <zhenwei.liu@intel.com>

--- intel_extension_for_transformers/llm/runtime/graph/scripts/python_api_example.py
+++ intel_extension_for_transformers/llm/runtime/graph/scripts/python_api_example.py
@@ -16,10 +16,10 @@
 # limitations under the License.
 
 from intel_extension_for_transformers.transformers import AutoModel, WeightOnlyQuantConfig
-model_name = "/mnt/disk1/data2/zhenweil/models/chatglm2-6b"
+model_name = "mosaicml/mpt-7b"
 woq_config = WeightOnlyQuantConfig(compute_dtype="int8")
 
-model = AutoModel.from_pretrained(model_name, quantization_config=woq_config, use_llm_runtime=True, trust_remote_code=True)
+model = AutoModel.from_pretrained(model_name, quantization_config=woq_config, use_llm_runtime=True)
 
-prompt = "你好"
+prompt = "Once upon a time, a little girl"
 print(model.generate(prompt, max_new_tokens=30))
