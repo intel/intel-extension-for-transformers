0bcb36b666690573e34f8954915caa7fc4a5c35d
Wenxin Zhang <wenxin.zhang@intel.com>
2023-09-28 20:07:31 +0800

    disable bf16 finetuning on ICX
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- .github/workflows/script/chatbot/start_ft_mpt-7b-chat_mpi.sh
+++ .github/workflows/script/chatbot/start_ft_mpt-7b-chat_mpi.sh
@@ -3,7 +3,7 @@ hname=$(hostname -s)
 python3 workflows/chatbot/fine_tuning/instruction_tuning_pipeline/finetune_clm.py \
     --model_name_or_path mosaicml/mpt-7b-chat \
     --train_file .github/workflows/sample_data/alpaca_data_sample_45.json \
-    --bf16 True \
+    --bf16 False \
     --output_dir ./mpt_peft_finetuned_model \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
