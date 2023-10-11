9604d8d959424fdc3b3aa344ef3c8d65174167c0
liuzhenwei <109187816+zhenwei-intel@users.noreply.github.com>
2023-10-11 12:31:09 +0800

    update post process with num_beams and do_sample (#430)
    * use mpt post process
    
    Signed-off-by: zhenwei-intel <zhenwei.liu@intel.com>

--- intel_extension_for_transformers/llm/runtime/graph/__init__.py
+++ intel_extension_for_transformers/llm/runtime/graph/__init__.py
@@ -105,17 +105,18 @@ class Model:
 
         # TODO support multi batch
         assert input_ids.shape[0] == 1, "Unsupport multi-batch input ids."
-        self.generate_round += 1
-
         if streamer:
+            if self.generate_round == 0:
+                streamer.put(input_ids)
             while not self.is_token_end():
                 out = self.model.generate(input_ids = input_ids.tolist()[0])
                 streamer.put(torch.tensor([out]))
                 ret[0].extend(out)
-            return ret
         else:
             ret[0].extend(self.model.generate_tokens(input_ids = input_ids.tolist()[0]))
-            return ret
+        
+        self.generate_round += 1
+        return ret
 
     def is_token_end(self):
         return self.model.is_token_end()
