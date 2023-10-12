83fd83522aed02e6fb8d946d852802323d03d359
Wenxin Zhang <wenxin.zhang@intel.com>
2023-10-12 19:02:43 +0800

    change back to main docker
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
+++ .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
@@ -1,5 +1,6 @@
 master_node=$1
 slave_node=$2
+runner_name=$3
 prepare_script="$(cat << 'EOF'
 oneccl_bindings_for_pytorch_path=\$(python -c \"from oneccl_bindings_for_pytorch import cwd; print(cwd)\")
 source \$oneccl_bindings_for_pytorch_path/env/setvars.sh
@@ -8,10 +9,10 @@ export I_MPI_HYDRA_IFACE=eth0
 EOF
 )"
 # for launching mpirun from yaml
-docker exec "chatbotfinetune-mpi-s0" bash -c "cd /root/chatbot; echo \"source activate && conda activate neuralchat\" > bash_setup.sh; echo \"$prepare_script\" >> bash_setup.sh; echo export MASTER_ADDR=$master_node >> bash_setup.sh"
+docker exec "chatbot-${runner_name}finetune-mpi-s0" bash -c "cd /root/chatbot; echo \"source activate && conda activate neuralchat\" > bash_setup.sh; echo \"$prepare_script\" >> bash_setup.sh; echo export MASTER_ADDR=$master_node >> bash_setup.sh"
 # for ssh setup mpi and oneccl properly
-docker exec "chatbotfinetune-mpi-s0" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
-docker exec "chatbotfinetune-mpi-s1" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
+docker exec "chatbot-${runner_name}finetune-mpi-s0" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
+docker exec "chatbot-${runner_name}finetune-mpi-s1" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
 
 echo "$master_node" > ./hosts2
 echo "$slave_node" >> ./hosts2
