9ae3e735039d5f9e6c21cd14e9d4a116c924770e
Wang, Chang <chang1.wang@intel.com>
2023-10-13 11:53:23 +0800

    Update modeling_auto.py

--- intel_extension_for_transformers/transformers/modeling/modeling_auto.py
+++ intel_extension_for_transformers/transformers/modeling/modeling_auto.py
@@ -57,7 +57,8 @@ class _BaseQBitsAutoModelClass:
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
-        import intel_extension_for_transformers.transformers.modeling.modeling_map
+        if isinstance(quantization_config, SmoothQuantConfig):
+            import intel_extension_for_transformers.transformers.modeling.modeling_map
         load_in_8bit = kwargs.pop("load_in_8bit", False)
         load_in_4bit = kwargs.pop("load_in_4bit", False)
         quantization_config = kwargs.pop("quantization_config", None)
