82702bd07e1649a5f8fb173cedc843980cc795ae
Wenxin Zhang <wenxin.zhang@intel.com>
2023-10-15 14:04:59 +0800

    mpirun failed fix
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
+++ .github/workflows/script/chatbot/prepare_ft_mpt-7b-chat_mpi.sh
@@ -11,8 +11,8 @@ EOF
 # for launching mpirun from yaml
 docker exec "chatbot-${runner_name}finetune-mpi-s0" bash -c "cd /root/chatbot; echo \"source activate && conda activate neuralchat\" > bash_setup.sh; echo \"$prepare_script\" >> bash_setup.sh; echo export MASTER_ADDR=$master_node >> bash_setup.sh"
 # for ssh setup mpi and oneccl properly
-docker exec "chatbot-${runner_name}finetune-mpi-s0" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
-docker exec "chatbot-${runner_name}finetune-mpi-s1" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc"
+docker exec "chatbot-${runner_name}finetune-mpi-s0" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc; lamboot;"
+docker exec "chatbot-${runner_name}finetune-mpi-s1" bash -c "echo \"$prepare_script\" >> ~/.bashrc; echo export MASTER_ADDR=$master_node >> ~/.bashrc; lamboot;"
 
 echo "$master_node" > ./hosts2
 echo "$slave_node" >> ./hosts2
