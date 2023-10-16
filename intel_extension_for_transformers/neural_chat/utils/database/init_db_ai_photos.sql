9b296a8e2d8ad5d2c118c4ff3abaa0a955ffded5
LetongHan <letong.han@intel.com>
2023-10-16 10:25:41 +0800

    add mysql utils for photoai apis
    Signed-off-by: LetongHan <letong.han@intel.com>

---
+++ intel_extension_for_transformers/neural_chat/utils/database/init_db_ai_photos.sql
@@ -0,0 +1,62 @@
+/*
+Create the Neural Chat Database: ai_photos
+Create empty Tables: user_info, image_info, face_info, image_face
+*/
+
+drop database if exists ai_photos;
+create database ai_photos;
+use ai_photos;
+
+drop table if exists user_info;
+create table user_info
+(
+  user_id varchar(20) primary key,
+  login_time datetime,
+  leave_time datetime,
+  is_active int,
+  unique (user_id)
+);
+
+drop table if exists image_info;
+create table image_info
+(
+  image_id int unsigned primary key auto_increment,
+  user_id varchar(20),
+  image_path varchar(100),
+  captured_time datetime,
+  caption varchar(200),
+  latitude varchar(20),
+  longitude varchar(20),
+  altitude varchar(20),
+  address varchar(200),
+  checked boolean,
+  other_tags varchar(200),
+  process_status varchar(20),
+  exist_status varchar(20),
+  unique (image_id),
+  FOREIGN KEY (user_id) REFERENCES user_info(user_id)
+);
+
+drop table if exists face_info;
+create table face_info
+(
+  face_id int unsigned primary key auto_increment,
+  face_tag varchar(20),
+  unique (face_id)
+);
+
+drop table if exists image_face;
+create table image_face
+(
+  image_face_id int unsigned primary key auto_increment,
+  image_id int unsigned,
+  image_path varchar(100),
+  face_id int unsigned,
+  xywh varchar(30),
+  user_id varchar(20),
+  face_tag varchar(20),
+  unique (image_face_id),
+  FOREIGN KEY (image_id) REFERENCES image_info(image_id),
+  FOREIGN KEY (face_id) REFERENCES face_info(face_id)
+);
+
