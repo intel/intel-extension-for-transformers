e19c2a7836a377c2a7fa00021af1b62de7fbd996
Wang, Chang <chang1.wang@intel.com>
2023-10-16 11:12:41 +0800

    Update modeling_auto.py
    Signed-off-by: Wang, Chang <chang1.wang@intel.com>

--- intel_extension_for_transformers/transformers/modeling/modeling_auto.py
+++ intel_extension_for_transformers/transformers/modeling/modeling_auto.py
@@ -58,6 +58,7 @@ class _BaseQBitsAutoModelClass:
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
         import intel_extension_for_transformers.transformers.modeling.modeling_map
+        print("replace modeling.py")
         load_in_8bit = kwargs.pop("load_in_8bit", False)
         load_in_4bit = kwargs.pop("load_in_4bit", False)
         quantization_config = kwargs.pop("quantization_config", None)
