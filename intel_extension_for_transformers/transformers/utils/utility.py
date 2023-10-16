6aa5bbf79dafbd181d57aeb67fb0fbe00088f573
changwangss <chang1.wang@intel.com>
2023-10-15 19:42:34 -0700

    add mistralai support
    Signed-off-by: changwangss <chang1.wang@intel.com>

--- intel_extension_for_transformers/transformers/utils/utility.py
+++ intel_extension_for_transformers/transformers/utils/utility.py
@@ -98,7 +98,8 @@ def generate_dummy_past_key_values(input_bs, model):
                 new_shape = [input_bs * num_attention_heads, 1, d_k]
             pkv = pkv + (torch.ones(size=new_shape),)
     else:
-        new_shape = [input_bs, num_attention_heads, 1, d_k]
+        #new_shape = [input_bs, num_attention_heads, 1, d_k]
+        new_shape = [input_bs, 8, 1, 128]
         dummy_tensor = torch.ones(size=new_shape)
         pkv = tuple(dummy_tensor for _ in range(nb_pkv))
     past_key_values = tuple(tuple(pkv) for _ in range(num_layers))
