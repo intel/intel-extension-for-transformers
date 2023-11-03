c6b627a7801a8b764db950ead82c2b5d2aa92a68
Cheng, Penghui <penghui.cheng@intel.com>
2023-11-03 21:40:39 +0800

    add UT for intel GPU ARC with weight only quantization
    Signed-off-by: Cheng, Penghui <penghui.cheng@intel.com>

--- tests/test_weight_only_gpu.py
+++ tests/test_weight_only_gpu.py
@@ -5,10 +5,14 @@ import shutil
 from intel_extension_for_transformers.transformers.modeling import AutoModelForCausalLM
 from intel_extension_for_transformers.transformers import WeightOnlyQuantConfig
 from transformers import AutoTokenizer
-from intel_extension_for_transformers.utils.utils import get_gpu_family
+from intel_extension_for_transformers.utils.utils import get_gpu_family, _ipex_available
 import torch.utils.data as data
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
+
+
+if _ipex_available:
+    gpu_name = get_gpu_family()
 
 
 class DummyDataset(data.Dataset):
@@ -38,6 +42,18 @@ class DummyDataset(data.Dataset):
             res = torch.tensor(input_ids),torch.tensor(self.encoded_dict['attention_mask'])
             return res
 
+
+class M(torch.nn.Module):
+    def __init__(self, with_bias=False):
+        super().__init__()
+        self.linear = torch.nn.Linear(32, 2, bias=with_bias)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+@unittest.skipIf(not _ipex_available or gpu_name != "max",
+    "There is no PVC(Max) GPU in this machine, skip this test!")
 class TestWeightOnly(unittest.TestCase):
 
     @classmethod
@@ -51,13 +67,7 @@ class TestWeightOnly(unittest.TestCase):
     def tearDownClass(cls) -> None:
         shutil.rmtree(cls.workspace, ignore_errors=True)
 
-
     def test_int4_ipex_pvc(self):
-        name = get_gpu_family()
-        if name != 'max':
-            print("There is no PVC (Max) GPU, skip this function {}".format('test_int4_ipex_pvc'))
-            return True
-
         from intel_extension_for_transformers.llm.quantization.utils import convert_to_quantized_model_by_ipex
         import intel_extension_for_pytorch as ipex
 
@@ -89,11 +99,6 @@ class TestWeightOnly(unittest.TestCase):
         return True
 
     def test_save_load_int4_ipex_pvc(self):
-        name = get_gpu_family()
-        if name != 'max':
-            print("There is no PVC (Max) GPU, skip this function {}".format('test_int4_ipex_pvc'))
-            return True
-
         from intel_extension_for_transformers.llm.quantization.utils import convert_to_quantized_model_by_ipex
         import intel_extension_for_pytorch as ipex
 
@@ -121,6 +126,9 @@ class TestWeightOnly(unittest.TestCase):
 
         return True
 
+
+@unittest.skipIf(not _ipex_available or gpu_name != "arc",
+    "There is no PVC(Max) GPU in this machine, skip this test!")
 class TestArcWeightOnly(unittest.TestCase):
 
     @classmethod
@@ -136,11 +144,6 @@ class TestArcWeightOnly(unittest.TestCase):
 
 
     def test_int4_ipex_arc(self):
-        name = get_gpu_family()
-        if name != 'arc':
-            print("There is no Arc GPU, skip this function {}".format('test_int4_ipex_arc'))
-            return True
-
         from intel_extension_for_transformers.llm.quantization.utils import convert_to_quantized_model
         import intel_extension_for_pytorch as ipex
 
@@ -170,6 +173,32 @@ class TestArcWeightOnly(unittest.TestCase):
         print("int4 logits {}".format(quan_logits.shape))
 
         return True
+
+    def test_int4_ipex_arc(self):
+        from intel_extension_for_transformers.llm.quantization.utils import convert_to_quantized_model
+        import intel_extension_for_pytorch as ipex
+        import intel_extension_for_transformers.gbits as gbits
+
+        raw_wei = torch.rand(2, 32, dtype=torch.float)
+        compress_wei = gbits.quantize(
+            raw_wei, True, 32, "fp32", "s4fullrange_scalef32")
+        revert_wei = torch.zeros(2, 32, dtype=torch.float)
+        gbits.dequantize(
+            compress_wei, revert_wei, True, "fp32", "s4fullrange_scalef32")
+        for bias in [True, False]:
+            model = M(with_bias=bias)
+            with torch.no_grad():
+                model.linear.weight = torch.nn.Parameter(revert_wei)
+            activation = torch.rand(1,32, dtype=torch.float)
+            output = model(activation)
+
+            config = WeightOnlyQuantConfig(weight_dtype="int4_fullrange", group_size=32)
+            convert_to_quantized_model(model, config, device="xpu")
+            output_quant = model(activation.to(torch.device("xpu")))
+            print(output)
+            print(output_quant)
+            assert torch.allclose(output, output_quant.to("cpu"), rtol=0.03)
+
 
 if __name__ == "__main__":
     unittest.main()
