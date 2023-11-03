f94b18ec88a0970ff77cfd9bc83184568856f099
Mengni Wang <mengni.wang@intel.com>
2023-11-03 14:53:18 +0800

    update gpu module
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- intel_extension_for_transformers/llm/quantization/nn/gpu/modules.py
+++ intel_extension_for_transformers/llm/quantization/nn/gpu/modules.py
@@ -18,14 +18,9 @@
 
 import os
 import torch
+import gbits
 from functools import reduce
 from operator import mul
-
-
-torch.ops.load_library(
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../../..", "libqbits.so")
-)
-
 
 class ParamsGBits(torch.nn.Parameter):
     def __new__(
@@ -102,10 +97,9 @@ class QuantizedLinearGPU(torch.nn.Linear):
         m = reduce(mul, shape[0:-1])
         out = torch.zeros(m, self.out_features, dtype=x.dtype)
         bias = None if self.bias is None else self.bias.data
-        torch.ops.weight_only_jblasop.qbits_linear(
+        gbits.linear(
             x.view(m, shape[-1]), self.weight.data, bias, out,
-            self.out_features, self.bias is not None, self.compute_dtype, self.weight_dtype
-        )
+            self.out_features, self.bias is not None, self.compute_dtype, self.weight_dtype)
         shape[-1] = self.out_features
         out = out.view(shape)
 
@@ -113,7 +107,7 @@ class QuantizedLinearGPU(torch.nn.Linear):
 
     def set_weights_bias(self, weight_data, bias=None):
         shape = weight_data.shape
-        weight = torch.ops.weight_only_jblasop.qbits_quantize(
+        weight = gbits.quantize(
             weight_data, True, self.blocksize, self.compute_dtype, self.weight_dtype)
         weight.resize_(shape)
         self.weight = ParamsGBits(
