1d45b428dea259b7a1843d26742db3e2a86f9751
Wenxin Zhang <wenxin.zhang@intel.com>
2023-11-07 18:06:09 +0800

    Revert "fix neuralchat ut (#631)"
    This reverts commit 65ab18f0901bfdadc6b2afffdebe892f9e197903.

--- intel_extension_for_transformers/neural_chat/tests/optimization/test_optimization.py
+++ intel_extension_for_transformers/neural_chat/tests/optimization/test_optimization.py
@@ -42,6 +42,18 @@ class TestChatbotBuilder(unittest.TestCase):
         print("\n")
         self.assertIsNotNone(response)
 
+    def test_build_chatbot_with_llm_runtime(self):
+        loading_config = LoadingModelConfig(use_llm_runtime=True)
+        config = PipelineConfig(model_name_or_path="facebook/opt-125m",
+            optimization_config=WeightOnlyQuantConfig(compute_dtype="int8", weight_dtype="int8"),
+            loading_config=loading_config
+        )
+        chatbot = build_chatbot(config)
+        self.assertIsNotNone(chatbot)
+        response = chatbot.predict(query="Tell me about Intel Xeon Scalable Processors.")
+        print(response)
+        self.assertIsNotNone(response)
+
     def test_build_chatbot_with_bitsandbytes_quant(self):
         if is_bitsandbytes_available() and torch.cuda.is_available():
             config = PipelineConfig(
@@ -60,17 +72,18 @@ class TestChatbotBuilder(unittest.TestCase):
             print(response)
             self.assertIsNotNone(response)
 
-    def test_build_chatbot_with_weight_only_quant(self):
-        loading_config = LoadingModelConfig(use_llm_runtime=False)
-        config = PipelineConfig(model_name_or_path="facebook/opt-125m",
-            optimization_config=WeightOnlyQuantConfig(compute_dtype="fp32", weight_dtype="int4_fullrange"),
-            loading_config=loading_config
-        )
-        chatbot = build_chatbot(config)
-        self.assertIsNotNone(chatbot)
-        response = chatbot.predict(query="Tell me about Intel Xeon Scalable Processors.")
-        print(response)
-        self.assertIsNotNone(response)
+    # run this case will cause core dump
+    # def test_build_chatbot_with_weight_only_quant(self):
+    #     loading_config = LoadingModelConfig(use_llm_runtime=False)
+    #     config = PipelineConfig(model_name_or_path="facebook/opt-125m",
+    #         optimization_config=WeightOnlyQuantConfig(compute_dtype="fp32", weight_dtype="int4_fullrange"),
+    #         loading_config=loading_config
+    #     )
+    #     chatbot = build_chatbot(config)
+    #     self.assertIsNotNone(chatbot)
+    #     response = chatbot.predict(query="Tell me about Intel Xeon Scalable Processors.")
+    #     print(response)
+    #     self.assertIsNotNone(response)
 
 if __name__ == '__main__':
     unittest.main()
