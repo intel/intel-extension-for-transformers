f336edd8f501316e14d0a0a326d9c66d0a36f613
sys-lpot-val <sys_lpot_val@intel.com>
2023-11-07 17:33:36 -0800

    update diffusers

--- examples/huggingface/pytorch/text-to-image/deployment/stable_diffusion/diffusion_utils_img2img.py
+++ examples/huggingface/pytorch/text-to-image/deployment/stable_diffusion/diffusion_utils_img2img.py
@@ -33,10 +33,11 @@ from diffusers.utils import (
     deprecate,
     is_accelerate_available,
     logging,
-    randn_tensor,
     replace_example_docstring,
 )
-from diffusers.pipeline_utils import DiffusionPipeline
+
+from diffusers.utils.torch_utils import randn_tensor
+from diffusers import DiffusionPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 
