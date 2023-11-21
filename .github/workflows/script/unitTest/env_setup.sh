3e417178cc11a58782b708000af567be0a568986
Wenxin Zhang <wenxin.zhang@intel.com>
2023-11-21 14:30:14 +0800

    update requirement
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- .github/workflows/script/unitTest/env_setup.sh
+++ .github/workflows/script/unitTest/env_setup.sh
@@ -19,7 +19,9 @@ until [ "$n" -ge 5 ]; do
 done
 
 # Install test requirements
+echo "Install Tests Requirements"
 cd $1 || exit 1
+pwd
 if [ -f "requirements.txt" ]; then
     python -m pip install --default-timeout=100 -r requirements.txt
     pip list
