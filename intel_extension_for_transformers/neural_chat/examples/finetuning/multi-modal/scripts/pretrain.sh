c8e92905672e32e14f7b3fa27b8e8252d6e373c1
lvkaokao <641553140@qq.com>
2023-11-27 10:27:33 -0500

    support llava multi-model training

---
+++ intel_extension_for_transformers/neural_chat/examples/finetuning/multi-modal/scripts/pretrain.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+deepspeed --include localhost:4,5 \
+    --master_port 29501 \
+    train.py \
+    --deepspeed scripts/zero2.json \
+    --model_name_or_path mistralai/Mistral-7B-v0.1 \
+    --template plain \
+    --data_path ./playground/data/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
+    --image_folder ./playground/data/LLaVA-Pretrain/images \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --tune_mm_mlp_adapter True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir ./llava-v1.5-mistral-7b-pretrain \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 24000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-3 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
