393e27675b8d4233561d16aab4ce99e4f2c4e667
lvkaokao <641553140@qq.com>
2023-11-29 02:09:20 -0500

    add multi-modal ut.

--- intel_extension_for_transformers/transformers/modeling/llava_models/multimodal_encoder/builder.py
+++ intel_extension_for_transformers/transformers/modeling/llava_models/multimodal_encoder/builder.py
@@ -24,5 +24,6 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     is_absolute_path_exists = os.path.exists(vision_tower)
     if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion"):
         return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-
-    raise ValueError(f'Unknown vision tower: {vision_tower}')
+    else:
+        print(f"Unknown vision tower: {vision_tower}")
+        return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
