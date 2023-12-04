990a0e7268fd160a759e0426796b10616efc508f
luoyu-intel <yu.luo@intel.com>
2023-12-04 10:53:44 +0800

    update int kblock launcher

--- intel_extension_for_transformers/llm/runtime/graph/core/layers/jblas_defs.h
+++ intel_extension_for_transformers/llm/runtime/graph/core/layers/jblas_defs.h
@@ -19,9 +19,10 @@ using tLauncher_Fp_F32F32 =
                                          jblas::epilogue::gemm::AccumulatorWriteBackFp32>;
 
 template <class GemmCore_T, template <class, JBLAS_ISA> class Wei_T>
-using tLauncher_Int8_F32F32 = jblas::wrapper::gemm::LauncherKBlock<
-    GemmCore_T::ISA, GemmCore_T, jblas::prologue_a::gemm::ActivationF32KBlockQuantize, Wei_T,
-    jblas::epilogue::gemm::CompInt8BlockEpilogue, jblas::epilogue::gemm::AccumulatorWriteBackFp32>;
+using tLauncher_Int8_F32F32 =
+    jblas::wrapper::gemm::LauncherIntKBlock<GemmCore_T::ISA, GemmCore_T,
+                                            jblas::prologue_a::gemm::ActivationF32KBlockQuantize, Wei_T,
+                                            jblas::epilogue::gemm::AccumulatorWriteBackFp32>;
 
 using tAVX2 = jblas::gemm::SCoreRowNAvx2<24, 4>;
 using tAVX_VNNI = jblas::gemm::ICoreRowNAvxvnni<24, 4>;
@@ -29,8 +30,13 @@ using tAVX512F = jblas::gemm::SCoreRowNAvx512f<48, 8>;
 using tAVX512_VNNI = jblas::gemm::ICoreRowNAvx512vnni<48, 8>;
 using tAMX_BF16 = jblas::gemm::HCoreRowNAmxbf16<64, 16>;
 using tAVX512_FP16 = jblas::gemm::HCoreRowNAvx512fp16<96, 8>;
-using tAMX_INT8_US = jblas::gemm::ICoreRowNAmxint8<64, 16>;
-using tAMX_INT8_SS = jblas::gemm::ICoreRowNAmxint8SS<64, 16>;
+using tAMX_INT8_US = jblas::gemm::ICoreRowNAmxint8<48, 16>;
+using tAMX_INT8_SS = jblas::gemm::ICoreRowNAmxint8SS<48, 16>;
+
+using tAVX_VNNI_KBlock = jblas::gemm::ICoreRowNAvxvnniKBlock<24, 4>;
+using tAVX512_VNNI_KBlock = jblas::gemm::ICoreRowNAvx512vnniKBlock<48, 8>;
+using tAMX_INT8_US_KBlock = jblas::gemm::ICoreRowNAmxint8KBlock<48, 16>;
+using tAMX_INT8_SS_KBlock = jblas::gemm::ICoreRowNAmxint8SSKBlock<48, 16>;
 
 template <class GC_T, JBLAS_ISA ISA_T>
 using tWeiNInt = jblas::prologue_b::gemm::WeightKBlockNInteger<GC_T, ISA_T>;
