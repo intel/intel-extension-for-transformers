bcfc4cff55773029091bcdcedf839a7d6d2efa57
Yu, Zhentao <zhentao.yu@intel.com>
2023-12-04 03:30:25 +0000

    fix starcoder cpplint
    Signed-off-by: Yu, Zhentao <zhentao.yu@intel.com>

--- intel_extension_for_transformers/llm/runtime/graph/models/starcoder/starcoder_utils.cpp
+++ intel_extension_for_transformers/llm/runtime/graph/models/starcoder/starcoder_utils.cpp
@@ -126,7 +126,7 @@ void STARCODER::load(model_context* ctx, model_progress_callback progress_callba
   size_t vram_total = 0;
 
   for (uint32_t i = 0; i < n_layer; ++i) {
-    const ne_backend backend = int(i) < i_gpu_start ? NE_BACKEND_CPU : MODEL_BACKEND_OFFLOAD;
+    const ne_backend backend = static_cast<int>(i) < i_gpu_start ? NE_BACKEND_CPU : MODEL_BACKEND_OFFLOAD;
     auto& layer = model.layers[i];
     std::string layers_i = "model/h" + std::to_string(i);
 
