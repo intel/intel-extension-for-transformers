cf5cb08f5e5fa71751e88044a40675fdcd75923f
Zhenzhong1 <root@JF5300-17A0901T.amr.corp.intel.com>
2023-12-06 20:06:56 -0800

    parser hyperparameters ok

--- intel_extension_for_transformers/llm/runtime/graph/models/model_utils/model_files.h
+++ intel_extension_for_transformers/llm/runtime/graph/models/model_utils/model_files.h
@@ -79,6 +79,43 @@ struct model_load_tensor_shard {
 
 enum model_split_type { SPLIT_NONE, SPLIT_BY_COLUMNS, SPLIT_BY_ROWS, TP_1D_ROW, TP_1D_COLUMN, TP_1D_ONLY_MASTER };
 
+struct gguf_header {
+    char magic[4];
+    uint32_t version;
+    uint64_t n_tensors; // GGUFv2
+    uint64_t n_kv;      // GGUFv2
+};
+
+struct gguf_context {
+    struct gguf_header header;
+
+    struct gguf_kv          * kv;
+    struct gguf_tensor_info * infos;
+
+    size_t alignment;
+    size_t offset;    // offset of `data` from beginning of file
+    size_t size;      // size of `data` in bytes
+
+    //uint8_t * padding;
+    void * data;
+};
+
+#define GGUF_GET_KEY(ctx, dst, func, type, req, key) \
+do { \
+    const std::string skey(key); \
+    const int kid = gguf_find_key(ctx, skey.c_str()); \
+    if (kid >= 0) { \
+        enum gguf_type ktype = gguf_get_kv_type(ctx, kid); \
+        if (ktype != (type)) { \
+            throw std::runtime_error(format("key %s has wrong type: %s", skey.c_str(), gguf_type_name(ktype))); \
+        } \
+        (dst) = func(ctx, kid); \
+    } else if (req) { \
+        throw std::runtime_error(format("key not found in model: %s", skey.c_str())); \
+    } \
+} while (0)
+
+
 struct model_load_tensor {
   std::vector<model_load_tensor_shard> shards;
 
@@ -211,6 +248,43 @@ struct model_load_tensors_map {
   std::unordered_map<std::string, size_t> name_to_idx;
 };
 
+#if UINTPTR_MAX == 0xFFFFFFFF
+    #define GGML_MEM_ALIGN 4
+#else
+    #define GGML_MEM_ALIGN 16
+#endif
+
+inline static void * ggml_aligned_malloc(size_t size) {
+    if (size == 0) {
+        printf("WARNING: Behavior may be unexpected when allocating 0 bytes for ggml_aligned_malloc!\n");
+        return NULL;
+    }
+    void * aligned_memory = NULL;
+#ifdef GGML_USE_CPU_HBM
+    int result = hbw_posix_memalign(&aligned_memory, 16, size);
+#elif GGML_USE_METAL
+    int result = posix_memalign(&aligned_memory, sysconf(_SC_PAGESIZE), size);
+#else
+    int result = posix_memalign(&aligned_memory, GGML_MEM_ALIGN, size);
+#endif
+    if (result != 0) {
+        // Handle allocation failure
+        const char *error_desc = "unknown allocation error";
+        switch (result) {
+            case EINVAL:
+                error_desc = "invalid alignment value";
+                break;
+            case ENOMEM:
+                error_desc = "insufficient memory";
+                break;
+        }
+        printf("%s: %s (attempted to allocate %6.2f MB)\n", __func__, error_desc, size/(1024.0*1024.0));
+        return NULL;
+    }
+    return aligned_memory;
+}
+#define GGML_ALIGNED_MALLOC(size) ggml_aligned_malloc(size)
+
 struct model_file_loader {
   model_file file;
   model_file_version file_version;
@@ -219,12 +293,262 @@ struct model_file_loader {
 
   model_file_loader(const char* fname, size_t file_idx, model_load_tensors_map& tensors_map) : file(fname, "rb") {
     fprintf(stderr, "model.cpp: loading model from %s\n", fname);
+    read_gguf();
     read_magic();
     read_hparams();
     read_vocab();
     read_tensor_metadata(file_idx, tensors_map);
   }
-  void read_magic() {
+
+
+// static const size_t GGUF_TYPE_SIZE[GGUF_TYPE_COUNT] = {
+//     [GGUF_TYPE_UINT8]   = sizeof(uint8_t),
+//     [GGUF_TYPE_INT8]    = sizeof(int8_t),
+//     [GGUF_TYPE_UINT16]  = sizeof(uint16_t),
+//     [GGUF_TYPE_INT16]   = sizeof(int16_t),
+//     [GGUF_TYPE_UINT32]  = sizeof(uint32_t),
+//     [GGUF_TYPE_INT32]   = sizeof(int32_t),
+//     [GGUF_TYPE_FLOAT32] = sizeof(float),
+//     [GGUF_TYPE_BOOL]    = sizeof(bool),
+//     [GGUF_TYPE_STRING]  = sizeof(struct gguf_str),
+//     [GGUF_TYPE_UINT64]  = sizeof(uint64_t),
+//     [GGUF_TYPE_INT64]   = sizeof(int64_t),
+//     [GGUF_TYPE_FLOAT64] = sizeof(double),
+//     [GGUF_TYPE_ARRAY]   = 0, // undefined
+// };
+
+  static bool gguf_fread_el(FILE * file, void * dst, size_t size, size_t * offset) {
+      const size_t n = fread(dst, 1, size, file);
+      *offset += n;
+      return n == size;
+  }
+
+        struct gguf_str {
+        uint64_t n;  // GGUFv2
+        char * data;
+    };
+
+static bool gguf_fread_str(FILE * file, struct gguf_str * p, size_t * offset) {
+    p->n    = 0;
+    p->data = NULL;
+
+    bool ok = true;
+
+    ok = ok && gguf_fread_el(file, &p->n,    sizeof(p->n), offset); p->data = reinterpret_cast<char *>(calloc(p->n + 1, 1));
+    ok = ok && gguf_fread_el(file,  p->data, p->n,         offset);
+
+    return ok;
+}
+  
+  void read_gguf() {
+
+
+
+    enum gguf_type {
+        GGUF_TYPE_UINT8   = 0,
+        GGUF_TYPE_INT8    = 1,
+        GGUF_TYPE_UINT16  = 2,
+        GGUF_TYPE_INT16   = 3,
+        GGUF_TYPE_UINT32  = 4,
+        GGUF_TYPE_INT32   = 5,
+        GGUF_TYPE_FLOAT32 = 6,
+        GGUF_TYPE_BOOL    = 7,
+        GGUF_TYPE_STRING  = 8,
+        GGUF_TYPE_ARRAY   = 9,
+        GGUF_TYPE_UINT64  = 10,
+        GGUF_TYPE_INT64   = 11,
+        GGUF_TYPE_FLOAT64 = 12,
+        GGUF_TYPE_COUNT,       // marks the end of the enum
+    };
+
+
+        union gguf_value {
+        uint8_t  uint8;
+        int8_t   int8;
+        uint16_t uint16;
+        int16_t  int16;
+        uint32_t uint32;
+        int32_t  int32;
+        float    float32;
+        uint64_t uint64;
+        int64_t  int64;
+        double   float64;
+        bool     bool_;
+
+        struct gguf_str str;
+
+        struct {
+            enum gguf_type type;
+
+            uint64_t n;  // GGUFv2
+            void * data;
+        } arr;
+    };
+
+    struct gguf_kv {
+      struct gguf_str key;
+
+      enum  gguf_type  type;
+      union gguf_value value;
+    };
+
+    struct gguf_header {
+        char magic[4];
+        uint32_t version;
+        uint64_t n_tensors; // GGUFv2
+        uint64_t n_kv;      // GGUFv2
+    };
+
+    struct gguf_context {
+      struct gguf_header header;
+
+      struct gguf_kv          * kv;
+      struct gguf_tensor_info * infos;
+
+      size_t alignment;
+      size_t offset;    // offset of `data` from beginning of file
+      size_t size;      // size of `data` in bytes
+
+      //uint8_t * padding;
+      void * data;
+    };
+
+    const char* name="/root/zhenzhong/gguf/intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime/graph/ne-chatglm2-fp32.bin.gguf";
+    FILE * file_gguf = fopen(name, "rb");
+    if (!file_gguf) {
+        return;
+    }
+
+
+    size_t offset = 0;
+    char magic[4];
+    gguf_fread_el(file_gguf, &magic, sizeof(magic), &offset);
+    std::cout << magic << std::endl;
+
+
+
+    //struct gguf_context * ctx = GGML_ALIGNED_MALLOC(sizeof(struct gguf_context));
+      int size = sizeof(struct gguf_context);
+      if (size == 0) {
+        printf("WARNING: Behavior may be unexpected when allocating 0 bytes for ggml_aligned_malloc!\n");
+    }
+    void * aligned_memory = NULL;
+    int result = posix_memalign(&aligned_memory, GGML_MEM_ALIGN, size);
+    if (result != 0) {
+        // Handle allocation failure
+        const char *error_desc = "unknown allocation error";
+        switch (result) {
+            case EINVAL:
+                error_desc = "invalid alignment value";
+                break;
+            case ENOMEM:
+                error_desc = "insufficient memory";
+                break;
+        }
+        printf("%s: %s (attempted to allocate %6.2f MB)\n", __func__, error_desc, size/(1024.0*1024.0));
+    }
+    //return aligned_memory;
+    struct gguf_context * ctx =  reinterpret_cast<struct gguf_context *>(aligned_memory);
+
+    strncpy(ctx->header.magic, magic, 4);
+
+    bool ok = true;
+    ctx->kv    = NULL;
+    ctx->infos = NULL;
+    ctx->data  = NULL;
+
+    ok = ok && gguf_fread_el(file_gguf, &ctx->header.version,   sizeof(ctx->header.version),   &offset);
+    ok = ok && gguf_fread_el(file_gguf, &ctx->header.n_tensors, sizeof(ctx->header.n_tensors), &offset);
+    ok = ok && gguf_fread_el(file_gguf, &ctx->header.n_kv,      sizeof(ctx->header.n_kv),      &offset);
+    
+    
+    if (ctx->header.version == 1) {
+        fprintf(stderr, "%s: GGUFv1 is no longer supported. please use a more up-to-date version\n", __func__);
+        // fclose(file);
+        // gguf_free(ctx);
+        // return NULL;
+    }
+
+    if (!ok) {
+        fprintf(stderr, "%s: failed to read header\n", __func__);
+        // fclose(file);
+        // gguf_free(ctx);
+        // return NULL;
+    }
+
+    // read the kv pairs
+    ctx->kv = reinterpret_cast<struct gguf_kv *>(malloc(ctx->header.n_kv * sizeof(struct gguf_kv)));
+
+    for (uint64_t i = 0; i < ctx->header.n_kv; ++i) {
+        struct gguf_kv * kv = &ctx->kv[i];
+
+
+        ok = ok && gguf_fread_str(file_gguf, &kv->key,                    &offset);
+        ok = ok && gguf_fread_el (file_gguf, &kv->type, sizeof(kv->type), &offset);
+
+    switch (kv->type) {
+        case GGUF_TYPE_UINT8:   ok = ok && gguf_fread_el (file_gguf, &kv->value.uint8,   sizeof(kv->value.uint8),   &offset); break;
+        case GGUF_TYPE_INT8:    ok = ok && gguf_fread_el (file_gguf, &kv->value.int8,    sizeof(kv->value.int8),    &offset); break;
+        case GGUF_TYPE_UINT16:  ok = ok && gguf_fread_el (file_gguf, &kv->value.uint16,  sizeof(kv->value.uint16),  &offset); break;
+        case GGUF_TYPE_INT16:   ok = ok && gguf_fread_el (file_gguf, &kv->value.int16,   sizeof(kv->value.int16),   &offset); break;
+        case GGUF_TYPE_UINT32:  ok = ok && gguf_fread_el (file_gguf, &kv->value.uint32,  sizeof(kv->value.uint32),  &offset); break;
+        case GGUF_TYPE_INT32:   ok = ok && gguf_fread_el (file_gguf, &kv->value.int32,   sizeof(kv->value.int32),   &offset); break;
+        case GGUF_TYPE_FLOAT32: ok = ok && gguf_fread_el (file_gguf, &kv->value.float32, sizeof(kv->value.float32), &offset); break;
+        case GGUF_TYPE_UINT64:  ok = ok && gguf_fread_el (file_gguf, &kv->value.uint64,  sizeof(kv->value.uint64),  &offset); break;
+        case GGUF_TYPE_INT64:   ok = ok && gguf_fread_el (file_gguf, &kv->value.int64,   sizeof(kv->value.int64),   &offset); break;
+        case GGUF_TYPE_FLOAT64: ok = ok && gguf_fread_el (file_gguf, &kv->value.float64, sizeof(kv->value.float64), &offset); break;
+        case GGUF_TYPE_BOOL:    ok = ok && gguf_fread_el (file_gguf, &kv->value.bool_,   sizeof(kv->value.bool_),   &offset); break;
+        case GGUF_TYPE_STRING:  ok = ok && gguf_fread_str(file_gguf, &kv->value.str,                                &offset); break;
+        case GGUF_TYPE_ARRAY:
+            {
+                ok = ok && gguf_fread_el(file_gguf, &kv->value.arr.type, sizeof(kv->value.arr.type), &offset);
+                ok = ok && gguf_fread_el(file_gguf, &kv->value.arr.n,    sizeof(kv->value.arr.n), &offset);
+
+                switch (kv->value.arr.type) {
+                    case GGUF_TYPE_UINT8:
+                    case GGUF_TYPE_INT8:
+                    case GGUF_TYPE_UINT16:
+                    case GGUF_TYPE_INT16:
+                    case GGUF_TYPE_UINT32:
+                    case GGUF_TYPE_INT32:
+                    case GGUF_TYPE_FLOAT32:
+                    case GGUF_TYPE_UINT64:
+                    case GGUF_TYPE_INT64:
+                    case GGUF_TYPE_FLOAT64:break;
+                    // case GGUF_TYPE_BOOL:
+                    //     {
+                    //         // TODO BOOL
+                    //         kv->value.arr.data = malloc(kv->value.arr.n * GGUF_TYPE_SIZE[kv->value.arr.type]);
+                    //         ok = ok && gguf_fread_el(file_gguf, kv->value.arr.data, kv->value.arr.n * GGUF_TYPE_SIZE[kv->value.arr.type], &offset);
+                    //     } break;
+                    case GGUF_TYPE_STRING:
+                        {
+                            kv->value.arr.data = malloc(kv->value.arr.n * sizeof(struct gguf_str));
+                            for (uint64_t j = 0; j < kv->value.arr.n; ++j) {
+                                ok = ok && gguf_fread_str(file_gguf, &((struct gguf_str *) kv->value.arr.data)[j], &offset);
+                            }
+                        } break;
+                    case GGUF_TYPE_ARRAY:
+                    case GGUF_TYPE_COUNT: printf("False && invalid type"); break; // GGML_ASSERT(false && "invalid type"); break;
+                }
+            } break;
+        case GGUF_TYPE_COUNT: printf("False && invalid type");  //GGML_ASSERT(false && "invalid type");
+    }
+
+    // if (!ok) {
+    //     break;
+    // }
+
+
+    }
+
+
+
+
+    return;
+  }
+
+  void read_magic() {    
     uint32_t magic = file.read_u32();
 
     if (magic == MODEL_FILE_MAGIC_NE) {
@@ -258,7 +582,12 @@ struct model_file_loader {
 
     throw format("unknown (magic, version) combination: %08x, %08x; is this really a NE file?", magic, version);
   }
+
+
+
   void read_hparams() {
+
+
     hparams.n_vocab = file.read_u32();
     hparams.n_embd = file.read_u32();
     hparams.n_mult = file.read_u32();
