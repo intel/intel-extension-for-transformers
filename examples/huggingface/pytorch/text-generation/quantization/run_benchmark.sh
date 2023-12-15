e2ebcab2a7d5622423300d3dcfdafca31c9f4298
Wang, Chang <chang1.wang@intel.com>
2023-12-15 20:58:40 +0800

    Fix llama series with transformers 4.36 (#941)

--- examples/huggingface/pytorch/text-generation/quantization/run_benchmark.sh
+++ examples/huggingface/pytorch/text-generation/quantization/run_benchmark.sh
@@ -119,6 +119,35 @@ function run_benchmark {
         model_name_or_path="THUDM/chatglm-6b"
         extra_cmd=$extra_cmd" --trust_remote_code True"
         pip install transformers==4.33
+    elif [ "${topology}" = "falcon_7b" ]; then
+        model_name_or_path="tiiuae/falcon-7b-instruct"
+        pip install transformers==4.33
+    elif [ "${topology}" = "baichuan_7b" ]; then
+        model_name_or_path="baichuan-inc/Baichuan-7B"
+        extra_cmd=$extra_cmd" --trust_remote_code True"
+        pip install transformers==4.33
+    elif [ "${topology}" = "baichuan_13b" ]; then
+        model_name_or_path="baichuan-inc/Baichuan-13B-Base"
+        extra_cmd=$extra_cmd" --trust_remote_code True"
+        extra_cmd=$extra_cmd" --revision 14d5b0e204542744900f6fb52422c6d633bdcb00"
+        pip install transformers==4.33
+    elif [ "${topology}" = "baichuan2_7b" ]; then
+        model_name_or_path="baichuan-inc/Baichuan2-7B-Base"
+        extra_cmd=$extra_cmd" --trust_remote_code True"
+        pip install transformers==4.33
+    elif [ "${topology}" = "baichuan2_13b" ]; then
+        model_name_or_path="baichuan-inc/Baichuan2-13B-Base"
+        extra_cmd=$extra_cmd" --trust_remote_code True"
+        pip install transformers==4.33
+    elif [ "${topology}" = "qwen_7b" ]; then
+        model_name_or_path="Qwen/Qwen-7B"
+        extra_cmd=$extra_cmd" --trust_remote_code True"
+    elif [ "${topology}" = "mistral_7b" ]; then
+        model_name_or_path="Intel/neural-chat-7b-v3"
+    elif [ "${topology}" = "phi_1b" ]; then
+        model_name_or_path="susnato/phi-1_dev"
+    elif [ "${topology}" = "phi_1_5b" ]; then
+        model_name_or_path="susnato/phi-1_5_dev"
     fi
 
     if [[ ${int8} == "true" ]]; then
