0e3de641f24c5781c54f71a83ff250faba71846d
lvliang-intel <liang1.lv@intel.com>
2023-12-20 17:56:20 +0800

    fix ut issue
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- intel_extension_for_transformers/neural_chat/models/base_model.py
+++ intel_extension_for_transformers/neural_chat/models/base_model.py
@@ -163,7 +163,7 @@ class BaseModel(ABC):
         self.get_conv_template(self.model_name, config.task)
         if (self.conv_template.roles[0] in query and self.conv_template.roles[1] in query) or \
               "starcoder" in self.model_name.lower() or "codellama" in self.model_name.lower() or \
-              "codegen" in self.model_name.lower:
+              "codegen" in self.model_name.lower():
             query_include_prompt = True
 
         # plugin pre actions
