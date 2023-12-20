21f9b04ee061b95b82be8e9d8ac1853ac290161c
XuhuiRen <xuhui.ren@intel.com>
2023-12-20 09:17:14 +0800

    add
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>

--- intel_extension_for_transformers/langchain/vectorstores/chroma.py
+++ intel_extension_for_transformers/langchain/vectorstores/chroma.py
@@ -37,6 +37,8 @@ from langchain.vectorstores.chroma import Chroma as Chroma_origin
 from langchain_core.embeddings import Embeddings
 from langchain_core.utils import xor_args
 from langchain_core.vectorstores import VectorStore
+import chromadb
+import chromadb.config
 _DEFAULT_PERSIST_DIR = './output'
 _LANGCHAIN_DEFAULT_COLLECTION_NAME = "langchain"
 
@@ -44,14 +46,69 @@ _LANGCHAIN_DEFAULT_COLLECTION_NAME = "langchain"
 class Chroma(Chroma_origin):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        try:
-            import chromadb
-            import chromadb.config
-        except ImportError:
-            raise ImportError(
-                "Could not import chromadb python package. "
-                "Please install it with `pip install chromadb`."
-            )
+        
+    @classmethod
+    def from_texts(
+        cls: Type[Chroma],
+        texts: List[str],
+        embedding: Optional[Embeddings] = None,
+        metadatas: Optional[List[dict]] = None,
+        ids: Optional[List[str]] = None,
+        collection_name: str = _LANGCHAIN_DEFAULT_COLLECTION_NAME,
+        persist_directory: Optional[str] = None,
+        client_settings: Optional[chromadb.config.Settings] = None,
+        client: Optional[chromadb.Client] = None,
+        collection_metadata: Optional[Dict] = None,
+        **kwargs: Any,
+    ) -> Chroma:
+        """Create a Chroma vectorstore from a raw documents.
+
+        If a persist_directory is specified, the collection will be persisted there.
+        Otherwise, the data will be ephemeral in-memory.
+
+        Args:
+            texts (List[str]): List of texts to add to the collection.
+            collection_name (str): Name of the collection to create.
+            persist_directory (Optional[str]): Directory to persist the collection.
+            embedding (Optional[Embeddings]): Embedding function. Defaults to None.
+            metadatas (Optional[List[dict]]): List of metadatas. Defaults to None.
+            ids (Optional[List[str]]): List of document IDs. Defaults to None.
+            client_settings (Optional[chromadb.config.Settings]): Chroma client settings
+            collection_metadata (Optional[Dict]): Collection configurations.
+                                                  Defaults to None.
+
+        Returns:
+            Chroma: Chroma vectorstore.
+        """
+        chroma_collection = cls(
+            collection_name=collection_name,
+            embedding_function=embedding,
+            persist_directory=persist_directory,
+            client_settings=client_settings,
+            client=client,
+            collection_metadata=collection_metadata,
+        )
+        if ids is None:
+            ids = [str(uuid.uuid1()) for _ in texts]
+        if hasattr(
+            chroma_collection._client, "max_batch_size"
+        ):  # for Chroma 0.4.10 and above
+            from chromadb.utils.batch_utils import create_batches
+
+            for batch in create_batches(
+                api=chroma_collection._client,
+                ids=ids,
+                metadatas=metadatas,
+                documents=texts,
+            ):
+                chroma_collection.add_texts(
+                    texts=batch[3] if batch[3] else [],
+                    metadatas=batch[2] if batch[2] else None,
+                    ids=batch[0],
+                )
+        else:
+            chroma_collection.add_texts(texts=texts, metadatas=metadatas, ids=ids)
+        return chroma_collection
 
     @classmethod
     def from_documents(
@@ -122,14 +179,13 @@ class Chroma(Chroma_origin):
     ) -> Chroma:
         if not persist_directory:
             persist_directory = _DEFAULT_PERSIST_DIR
-        if sign == 'child':
-            persist_directory = persist_directory + "_child"
         if os.path.exists(persist_directory):
             if bool(os.listdir(persist_directory)):
                 print("Load the existing database!")
                 chroma_collection = cls(
                     collection_name=collection_name,
                     embedding_function=embedding,
+                    sign=sign,
                     persist_directory=persist_directory,
                     client_settings=client_settings,
                     client=client,
@@ -140,7 +196,7 @@ class Chroma(Chroma_origin):
         else:
             print("Create a new kb...")
             chroma_collection = cls.from_documents(
-                content=content,
+                documents=documents,
                 embedding=embedding,
                 ids=ids,
                 collection_name=collection_name,
