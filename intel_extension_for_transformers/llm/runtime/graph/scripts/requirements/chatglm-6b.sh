7d137c056c9c8d7887779ce8a9222b74a72c705a
Ding, Yi1 <yi1.ding@intel.com>
2023-12-26 16:38:24 +0800

    avoid variable contamination

--- intel_extension_for_transformers/llm/runtime/graph/scripts/requirements/chatglm-6b.sh
+++ intel_extension_for_transformers/llm/runtime/graph/scripts/requirements/chatglm-6b.sh
@@ -16,5 +16,4 @@
 #===============================================================================
 
 # To avoid the error: 'ChatGLMTokenizer' object has no attribute 'sp_tokenizer'
-script_dir=$(dirname "${BASH_SOURCE[0]}")
-pip install -r "$script_dir/common.txt" transformers==4.33.1
+pip install -r "$(dirname "${BASH_SOURCE[0]}")/common.txt" transformers==4.33.1
