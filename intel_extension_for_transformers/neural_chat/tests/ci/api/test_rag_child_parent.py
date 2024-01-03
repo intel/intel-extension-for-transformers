a8bd70537097a88febb8163262be3c0767922301
Liangyx2 <yuxiang.liang@intel.com>
2024-01-03 16:01:08 +0800

    add test_rag_child_parent.py
    Signed-off-by: Liangyx2 <yuxiang.liang@intel.com>

---
+++ intel_extension_for_transformers/neural_chat/tests/ci/api/test_rag_child_parent.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import os
+import shutil
+from intel_extension_for_transformers.neural_chat import build_chatbot
+from intel_extension_for_transformers.neural_chat import PipelineConfig
+from intel_extension_for_transformers.neural_chat import plugins
+
+# All UT cases use 'facebook/opt-125m' to reduce test time.
+
+class TestChatbotBuilder_child_parent(unittest.TestCase):
+    def setUp(self):
+        if os.path.exists("test_rag"):
+            shutil.rmtree("test_rag", ignore_errors=True)
+        if os.path.exists("test_rag_child"):
+            shutil.rmtree("test_rag_child", ignore_errors=True)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        if os.path.exists("test_rag"):
+            shutil.rmtree("test_rag", ignore_errors=True)
+        if os.path.exists("test_rag_child"):
+            shutil.rmtree("test_rag_child", ignore_errors=True)
+        return super().tearDown()
+
+    def test_retrieval_child_parent(self):
+        plugins.retrieval.enable = True
+        plugins.retrieval.args["input_path"] = "../assets/docs/sample.txt"
+        plugins.retrieval.args["persist_directory"] = "./test_rag"
+        plugins.retrieval.args["retrieval_type"] = "child_parent"
+        config = PipelineConfig(model_name_or_path="facebook/opt-125m",
+                                plugins=plugins)
+        chatbot = build_chatbot(config)
+        response = chatbot.predict("How many cores does the Intel Xeon Platinum 8480+ Processor have in total?")
+        print(response)
+        plugins.retrieval.args["persist_directory"] = "./output"
+        plugins.retrieval.args["retrieval_type"] = 'default'
+        self.assertIsNotNone(response)
+        plugins.retrieval.enable = False
+
+if __name__ == '__main__':
+    unittest.main()
