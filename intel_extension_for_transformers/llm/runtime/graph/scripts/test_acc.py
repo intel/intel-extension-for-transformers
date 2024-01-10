010bb75df097b07fb73d7df2a2818e1b452f0c7a
zhenwei-intel <zhenwei.liu@intel.com>
2024-01-10 10:05:07 +0800

    update model format
    Signed-off-by: zhenwei-intel <zhenwei.liu@intel.com>

--- intel_extension_for_transformers/llm/runtime/graph/scripts/test_acc.py
+++ intel_extension_for_transformers/llm/runtime/graph/scripts/test_acc.py
@@ -15,12 +15,13 @@ import sys
 from intel_extension_for_transformers.llm.evaluation.lm_eval import evaluate
 
 model_name = sys.argv[1]
+model_format = sys.argv[2]
 results = evaluate(
     model="hf-causal",
     model_args=f'pretrained="{model_name}"',
     tasks=["lambada_openai"],
     # limit=5,
-    model_format="runtime"
+    model_format=f"{model_format}"
 )
 
 print(results)
