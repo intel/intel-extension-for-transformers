204edc068194a392f381b807a7eb5ff02f6d93a6
Wenxin Zhang <wenxin.zhang@intel.com>
2024-01-10 15:55:56 +0800

    enable GLIBCXX_3.4.26
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- intel_extension_for_transformers/llm/runtime/graph/scripts/ci/cpp_graph_inference.sh
+++ intel_extension_for_transformers/llm/runtime/graph/scripts/ci/cpp_graph_inference.sh
@@ -267,7 +267,7 @@ function main() {
     if [[ "${compiler_version}" != "12.1.0" ]]; then
         conda install --update-deps -c conda-forge gxx==${compiler_version} gcc==${compiler_version} gxx_linux-64==${compiler_version} libstdcxx-ng sysroot_linux-64 -y
     fi
-
+    export LD_LIBRARY_PATH=${HOME}/miniconda3/envs/${conda_env}/lib/:$LD_LIBRARY_PATH
     # setup conda env for LLM
 
     # get cpu info
