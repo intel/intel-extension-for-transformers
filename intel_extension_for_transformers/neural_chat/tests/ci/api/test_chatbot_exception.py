aa75ae02979dc590278b8dacd6c7cc7dd95fb0cb
Liangyx2 <yuxiang.liang@intel.com>
2024-01-12 11:37:44 +0800

    Update test_chatbot_exception.py
    Signed-off-by: Liangyx2 <yuxiang.liang@intel.com>

--- intel_extension_for_transformers/neural_chat/tests/ci/api/test_chatbot_exception.py
+++ intel_extension_for_transformers/neural_chat/tests/ci/api/test_chatbot_exception.py
@@ -19,8 +19,19 @@ import unittest
 import torch
 from unittest.mock import MagicMock, patch
 from intel_extension_for_transformers.neural_chat import build_chatbot, PipelineConfig
-from intel_extension_for_transformers.transformers import MixedPrecisionConfig
 from intel_extension_for_transformers.neural_chat.utils.common import get_device_type
+from transformers import TrainingArguments, Seq2SeqTrainingArguments
+from intel_extension_for_transformers.neural_chat.config import (
+    ModelArguments,
+    DataArguments,
+    FinetuningArguments,
+    TextGenerationFinetuningConfig,
+)
+from intel_extension_for_transformers.neural_chat.chatbot import finetune_model
+import unittest
+import shutil
+import os
+
 class TestBuildChatbotExceptions(unittest.TestCase):
 
     @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
@@ -158,57 +169,62 @@ class TestBuildChatbotExceptions(unittest.TestCase):
 
     @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
     @patch('intel_extension_for_transformers.neural_chat.models.model_utils.load_model')
-    def test_adapter_load_model_value_error_unknown(self, mock_load_model):
-        # Test value error exception handling
-        config = PipelineConfig(model_name_or_path="facebook/opt-125m")
-        mock_load_model.side_effect = ValueError("load_model: unknown ValueError occurred")
-        result = build_chatbot(config)
-        self.assertIsNone(result)
-
-    @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
-    @patch('intel_extension_for_transformers.neural_chat.models.model_utils.load_model')
-    def test_adapter_load_model_environmentvalue_error_unknown(self, mock_load_model):
-        # Test value error exception handling
-        config = PipelineConfig(model_name_or_path="facebook/opt-125m")
-        mock_load_model.side_effect = ValueError("load_model: unknown EnvironmentError occurred")
-        result = build_chatbot(config)
-        self.assertIsNone(result)
-
-    @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
-    @patch('intel_extension_for_transformers.neural_chat.models.model_utils.load_model')
-    def test_adapter_load_model_error_unexpected(self, mock_load_model):
-        # Test value error exception handling
-        config = PipelineConfig(model_name_or_path="facebook/opt-125m")
-        mock_load_model.side_effect = ValueError("load_model: an unexpected error occurred")
-        result = build_chatbot(config)
-        self.assertIsNone(result)
-
-    @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
-    @patch('intel_extension_for_transformers.neural_chat.models.model_utils.load_model')
-    def test_adapter_load_model_unsupported_model(self, mock_load_model):
-        # Test value error exception handling
-        config = PipelineConfig(model_name_or_path="facebook/opt-125m")
-        mock_load_model.side_effect = ValueError("unsupported model name or path {model_name}")
-        result = build_chatbot(config)
-        self.assertIsNone(result)
-
-    @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
-    @patch('intel_extension_for_transformers.neural_chat.models.model_utils.load_model')
-    def test_adapter_load_model_unsupported_device(self, mock_load_model):
-        # Test value error exception handling
-        config = PipelineConfig(model_name_or_path="facebook/opt-125m")
-        mock_load_model.side_effect = ValueError("unsupported device {device}, only supports cpu, xpu, cuda and hpu now.")
-        result = build_chatbot(config)
-        self.assertIsNone(result)
-
-    @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
-    @patch('intel_extension_for_transformers.neural_chat.models.model_utils.load_model')
     def test_adapter_load_model_exception(self, mock_load_model):
         # Test generic exception handling
         config = PipelineConfig(model_name_or_path="facebook/opt-125m")
         mock_load_model.side_effect = Exception("Some generic error")
         result = build_chatbot(config)
         self.assertIsNone(result)
+
+json_data = \
+"""
+[
+    {"instruction": "Generate a slogan for a software company", "input": "", "output": "The Future of Software is Here"},
+    {"instruction": "Provide the word that comes immediately after the.", "input": "He threw the ball over the fence.", "output": "fence."}
+]
+"""
+test_data_file = './alpaca_test.json'
+
+class TestFinetuneExceptions(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.device = get_device_type()
+        with open(test_data_file, mode='w') as f:
+            f.write(json_data)
+
+            self.training_args = TrainingArguments(
+                    output_dir='./tmp',
+                    do_train=True,
+                    max_steps=3,
+                    overwrite_output_dir=True)
+
+            self.seq2seq_training_args = Seq2SeqTrainingArguments(
+                    output_dir='./tmp',
+                    do_train=True,
+                    max_steps=3,
+                    overwrite_output_dir=True)
+
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree('./tmp', ignore_errors=True)
+        os.remove(test_data_file)
+
+    @unittest.skipIf(get_device_type() != 'cpu', "Only run this test on CPU")
+    @patch('intel_extension_for_transformers.llm.finetuning.finetuning.Finetuning')
+    def test_finetune_error_dataset_not_found(self, mock_finetune):
+        model_args = ModelArguments(model_name_or_path="facebook/opt-125m")
+        mock_finetune.side_effect = FileNotFoundError("Couldn't find a dataset script")
+        data_args = DataArguments(train_file=test_data_file)
+        finetune_args = FinetuningArguments(device=self.device, do_lm_eval=False)
+        finetune_cfg = TextGenerationFinetuningConfig(
+            model_args=model_args,
+            data_args=data_args,
+            training_args=self.training_args,
+            finetune_args=finetune_args
+        )
+        self.assertRaises(FileNotFoundError,finetune_model,finetune_cfg)
+
 
 if __name__ == '__main__':
     unittest.main()
