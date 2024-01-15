b46ecbcba55d6906e7f46d6abf501adbc15d5668
Spycsh <sihan.chen@intel.com>
2024-01-14 23:21:08 -0800

    fix

--- intel_extension_for_transformers/neural_chat/server/neuralchat_server.py
+++ intel_extension_for_transformers/neural_chat/server/neuralchat_server.py
@@ -124,8 +124,8 @@ class NeuralChatServerExecutor(BaseCommandExecutor):
                         from ..pipeline.plugins.audio.tts import TextToSpeech
                         plugins[plugin_name]['class'] = TextToSpeech
                     elif plugin_name == "tts_multilang":
-                        from ..pipeline.plugins.audio.tts_multilang import ChineseTextToSpeech
-                        plugins[plugin_name]['class'] = ChineseTextToSpeech
+                        from ..pipeline.plugins.audio.tts_multilang import MultilangTextToSpeech
+                        plugins[plugin_name]['class'] = MultilangTextToSpeech
                     elif plugin_name == "asr":
                         from ..pipeline.plugins.audio.asr import AudioSpeechRecognition
                         plugins[plugin_name]['class'] = AudioSpeechRecognition
