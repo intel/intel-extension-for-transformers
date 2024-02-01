c24f8d152c3109550a7fe618e53d3a4494bbc12e
lvliang-intel <liang1.lv@intel.com>
2024-02-01 14:12:44 +0800

    Support DeciLM-7B and DeciLM-7B-instruct
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- intel_extension_for_transformers/neural_chat/chatbot.py
+++ intel_extension_for_transformers/neural_chat/chatbot.py
@@ -134,6 +134,9 @@ def build_chatbot(config: PipelineConfig=None):
     elif "solar" in config.model_name_or_path.lower():
         from .models.solar_model import SolarModel
         adapter = SolarModel(config.model_name_or_path, config.task)
+    elif "decilm" in config.model_name_or_path.lower():
+        from .models.decilm_model import DeciLMModel
+        adapter = DeciLMModel(config.model_name_or_path, config.task)
     elif "opt" in config.model_name_or_path.lower() or \
          "gpt" in config.model_name_or_path.lower() or \
          "flan-t5" in config.model_name_or_path.lower() or \
