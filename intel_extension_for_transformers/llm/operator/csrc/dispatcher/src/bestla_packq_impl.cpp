d9f36e56e58625c09c7b3302999ca6c5f6e71548
Wang,Zhe <zhe1.wang@intel.com>
2024-02-02 15:26:52 +0800

    packw info acquire done.

--- intel_extension_for_transformers/llm/operator/csrc/dispatcher/src/bestla_packq_impl.cpp
+++ intel_extension_for_transformers/llm/operator/csrc/dispatcher/src/bestla_packq_impl.cpp
@@ -17,16 +17,96 @@ void execute_qpack(woq_packq_param* p, woq_packq_ctx* ctx) {
                   p->asym ? ctx->zp->data_ptr<int8_t>() : nullptr, &qpackw, &dispatcher_utils::DefaultThreading);
 }
 
+std::string get_dtype_str(BTLA_DTYPE dtype) {
+  switch (dtype) {
+    case BTLA_DTYPE::F32:
+      return "fp32";
+    case BTLA_DTYPE::BF16:
+      return "bf16";
+    case BTLA_DTYPE::S4_CLIP:
+      return "int4_clip";
+    case BTLA_DTYPE::S4_FULLRANGE:
+      return "int4_fullrange";
+    case BTLA_DTYPE::F4_NF4:
+      return "nf4";
+    case BTLA_DTYPE::F4_E2M1:
+      return "f4_e2m1";
+    case BTLA_DTYPE::F4_BNB:
+      return "f4_e2m1_bnb";
+    case BTLA_DTYPE::S8:
+      return "int8";
+    case BTLA_DTYPE::F8_E5M2:
+      return "fp8_e5m2";
+    case BTLA_DTYPE::F8_E4M3:
+      return "fp8_e4m3";
+    case BTLA_DTYPE::F8_E8M0:
+      return "fp8_e8m0";
+    default:
+      TORCH_CHECK(false, "QBits: unrecognized data type.")
+      break;
+  }
+}
+
+std::string get_cmpt_str(bestla::gemm::CompType cmpt) {
+  using bestla::gemm::CompType;
+  switch (cmpt) {
+    case CompType::COMP_INT8_US_INT32:
+    case CompType::COMP_INT8_US_FP32:
+      return "int8";
+    case CompType::COMP_FP32:
+      return "fp32";
+    case CompType::COMP_BF16_FP32:
+      return "bf16";
+    default:
+      TORCH_CHECK(false, "QBits: unrecognized compute type.");
+      break;
+  }
+}
+
+std::vector<int> get_ascii_vec(std::string str) {
+  std::vector<int32_t> ret;
+  for (char c : str) ret.push_back(static_cast<int32_t>(c));
+  return ret;
+}
+
 torch::Tensor get_packw_info(torch::Tensor& packw, PACKW_ACQUIRE_TYPE ACQ_T) {
   torch::Tensor output;
-  auto packw_ptr = bestla::storage::gemm::PackedWeightParser::deserialBuffer(packw.data_ptr());
+  auto packw_ptr = dynamic_cast<bestla::storage::gemm::StorageWeightKBlockNInteger*>(
+      bestla::storage::gemm::PackedWeightParser::deserialBuffer(packw.data_ptr()));
+  output = torch::empty(1, torch::kInt64);
   switch (ACQ_T) {
     case SIZE:
-      output = torch::empty(1, torch::kInt64);
-      output.index_put_({0}, static_cast<int64_t>(packw_ptr->mSize));
-      break;
+      return output.index_put_({0}, static_cast<int64_t>(packw_ptr->mSize));
+    case BLOCKSIZE:
+      return output.index_put_({0}, static_cast<int64_t>(packw_ptr->mBlockSize));
+    case K:
+      return output.index_put_({0}, static_cast<int64_t>(packw_ptr->mK));
+    case N:
+      return output.index_put_({0}, static_cast<int64_t>(packw_ptr->mN));
+    case ACT_SHUFFLE:
+      return output.index_put_({0}, static_cast<int64_t>(packw_ptr->ShfIndice() != nullptr ? 1 : 0));
+    case G_IDX: {
+      auto tensor_size = packw_ptr->mShuffleIndices.size<int>();
+      TORCH_CHECK(packw_ptr->ShfIndice() != nullptr, "QBits: not pack g_idx tensor.");
+      output = torch::empty(tensor_size, torch::kInt32);
+      memcpy(output.data_ptr(), packw_ptr->ShfIndice(), tensor_size * sizeof(int));
+    } break;
+    case WEI_TYPE: {
+    }
+    case SCALE_TYPE: {
+      auto ascii_vec = get_ascii_vec(get_dtype_str(packw_ptr->mDType));
+      output = torch::empty(ascii_vec.size(), torch::kInt32);
+      memcpy(output.data_ptr(), ascii_vec.data(), ascii_vec.size() * sizeof(int));
+    } break;
+    case CMPT_TYPE: {
+      auto CType = bestla::gemm::CoreAttr::get_mask_val(packw_ptr->mCoreId, bestla::gemm::CoreAttr::COMP_MASK,
+                                                        bestla::gemm::CoreAttr::COMP_SHIFT);
+      auto ascii_vec = get_ascii_vec(get_cmpt_str(static_cast<bestla::gemm::CompType>(CType)));
+      output = torch::empty(ascii_vec.size(), torch::kInt32);
+      memcpy(output.data_ptr(), ascii_vec.data(), ascii_vec.size() * sizeof(int));
+    } break;
     default:
-      TORCH_CHECK(false, "unsupported acquire_type");
+      TORCH_CHECK(false, "QBits: unsupported acquire_type");
       break;
   }
   return output;
