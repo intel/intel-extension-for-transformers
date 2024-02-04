cf27b3f028eda1a37327751ad115e604d8bc947a
Dong, Bo1 <bo1.dong@intel.com>
2024-02-03 17:34:03 -0800

    change the name
    Signed-off-by: Dong, Bo1 <bo1.dong@intel.com>

--- .github/workflows/script/unitTest/run_unit_test_neuralspeed.sh
+++ .github/workflows/script/unitTest/run_unit_test_neuralspeed.sh
@@ -5,10 +5,10 @@ LOG_DIR=/intel-extension-for-transformers/log_dir
 mkdir -p ${LOG_DIR}
 WORKING_DIR="/intel-extension-for-transformers/intel_extension_for_transformers/llm/runtime/neural_speed"
 
-# -------------------LLM Runtime Test-------------------
-function llmruntime_test() {
+# -------------------Neural Speed Test-------------------
+function neuralspeed_test() {
     cd ${WORKING_DIR}/tests
-    local ut_log_name=${LOG_DIR}/unit_test_llm_runtime.log
+    local ut_log_name=${LOG_DIR}/unit_test_neural_speed.log
     find . -name "test*.py" | sed 's,\.\/,python ,g' | sed 's/$/ --verbose/' >run.sh
     # run UT
     $BOLD_YELLOW && echo "cat run.sh..." && $RESET
@@ -32,7 +32,7 @@ function llmruntime_test() {
 
 function main() {
     bash /intel-extension-for-transformers/.github/workflows/script/unitTest/env_setup.sh "${WORKING_DIR}"
-    llmruntime_test
+    neuralspeed_test
 }
 
 main
