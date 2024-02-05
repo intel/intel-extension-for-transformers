338bf39072a72d9da1c380f5af2d2bd875a6be66
yuwenzho <yuwen.zhou@intel.com>
2024-02-05 10:42:28 +0800

    upgrade sentense_transformers to 2.3.1
    Signed-off-by: yuwenzho <yuwen.zhou@intel.com>

--- intel_extension_for_transformers/langchain/embeddings/optimized_instructor_embedding.py
+++ intel_extension_for_transformers/langchain/embeddings/optimized_instructor_embedding.py
@@ -57,13 +57,17 @@ class OptimizedInstructor(InstructorEmbedding.INSTRUCTOR):
                          model_name_or_path,
                          token: Optional[Union[bool, str]],
                          cache_folder: Optional[str],
+                         revision: Optional[str] = None,
                          trust_remote_code: bool = False): # pragma: no cover
         """Creates a simple Transformer + Mean Pooling model and returns the modules."""
         logger.warning("No sentence-transformers model found with name {}." \
                        "Creating a new one with MEAN pooling.".format(model_name_or_path))
         transformer_model = OptimzedTransformer(
-            model_name_or_path, cache_dir=cache_folder, model_args={"token": token,
-                                                                    "trust_remote_code": trust_remote_code})
+            model_name_or_path,
+            cache_dir=cache_folder,
+            model_args={"token": token, "trust_remote_code": trust_remote_code, "revision": revision},
+            tokenizer_args={"token": token, "trust_remote_code": trust_remote_code, "revision": revision},
+            )
         pooling_model = sentence_transformers.models.Pooling(
             transformer_model.get_word_embedding_dimension(), 'mean')
         return [transformer_model, pooling_model]
@@ -72,11 +76,16 @@ class OptimizedInstructor(InstructorEmbedding.INSTRUCTOR):
                           model_name_or_path: str,
                           token: Optional[Union[bool, str]],
                           cache_folder: Optional[str],
+                          revision: Optional[str] = None,
                           trust_remote_code: bool = False):
         """Loads a full sentence-transformers model."""
         # Check if the config_sentence_transformers.json file exists (exists since v2 of the framework)
         config_sentence_transformers_json_path = sentence_transformers.util.load_file_path(
-            model_name_or_path, 'config_sentence_transformers.json', token=token, cache_folder=cache_folder)
+            model_name_or_path,
+            'config_sentence_transformers.json',
+            token=token,
+            cache_folder=cache_folder,
+            revision=revision)
         if config_sentence_transformers_json_path is not None:
             with open(config_sentence_transformers_json_path) as fIn:
                 self._model_config = json.load(fIn)
@@ -93,7 +102,7 @@ class OptimizedInstructor(InstructorEmbedding.INSTRUCTOR):
 
         # Check if a readme exists
         model_card_path = sentence_transformers.util.load_file_path(
-            model_name_or_path, 'README.md', token=token, cache_folder=cache_folder)
+            model_name_or_path, 'README.md', token=token, cache_folder=cache_folder, revision=revision,)
         if model_card_path is not None:
             try:
                 with open(model_card_path, encoding='utf8') as fIn:
@@ -103,13 +112,19 @@ class OptimizedInstructor(InstructorEmbedding.INSTRUCTOR):
 
         # Load the modules of sentence transformer
         modules_json_path = sentence_transformers.util.load_file_path(
-            model_name_or_path, 'modules.json', token=token, cache_folder=cache_folder)
+            model_name_or_path,
+            'modules.json',
+            token=token,
+            cache_folder=cache_folder,
+            revision=revision,)
         with open(modules_json_path) as fIn:
             modules_config = json.load(fIn)
 
         modules = OrderedDict()
         for module_config in modules_config:
-            if module_config['idx']==0:
+            module_class = sentence_transformers.util.import_from_string(module_config['type'])
+            print('module_class', module_class)
+            if module_class == sentence_transformers.models.Transformer and module_config['path'] == "":
                 logger.info('load Optimized InstructorTransformer')
                 kwargs = {}
                 for config_name in ['sentence_bert_config.json', 'sentence_roberta_config.json',
@@ -117,26 +132,47 @@ class OptimizedInstructor(InstructorEmbedding.INSTRUCTOR):
                                     'sentence_albert_config.json', 'sentence_xlm-roberta_config.json',
                                     'sentence_xlnet_config.json']:
                     config_path = sentence_transformers.util.load_file_path(
-                        model_name_or_path, config_name, token=token, cache_folder=cache_folder)
+                        model_name_or_path,
+                        config_name,
+                        token=token,
+                        cache_folder=cache_folder,
+                        revision=revision,)
                     if config_path is not None:
                         with open(config_path) as fIn:
                             kwargs = json.load(fIn)
                         break
+                hub_kwargs = {"token": token, "trust_remote_code": trust_remote_code, "revision": revision}
                 if "model_args" in kwargs:
-                    kwargs["model_args"]["token"] = token
-                    kwargs["model_args"]["trust_remote_code"] = trust_remote_code
+                    kwargs["model_args"].update(hub_kwargs)
                 else:
-                    kwargs["model_args"] = {"token": token, "trust_remote_code": trust_remote_code}
+                    kwargs["model_args"] = hub_kwargs
+                if "tokenizer_args" in kwargs:
+                    kwargs["tokenizer_args"].update(hub_kwargs)
+                else:
+                    kwargs["tokenizer_args"] = hub_kwargs
                 module = OptimizedInstructorTransformer(model_name_or_path, cache_dir=cache_folder, **kwargs)
-            elif module_config['idx']==1:
+            elif module_class == sentence_transformers.models.Pooling:
                 module_class = InstructorEmbedding.INSTRUCTOR_Pooling
-                module_path = get_module_path(
-                    model_name_or_path, module_config['path'], token, cache_folder)
+                module_path = sentence_transformers.util.load_dir_path(
+                    model_name_or_path,
+                    module_config['path'],
+                    token,
+                    cache_folder,
+                    revision=revision,
+                )
+                module = module_class.load(module_path)
+            elif module_class == sentence_transformers.models.Normalize:
+                module_path = None
                 module = module_class.load(module_path)
             else:
                 module_class = InstructorEmbedding.import_from_string(module_config['type'])
-                module_path = get_module_path(
-                    model_name_or_path, module_config['path'], token, cache_folder)
+                module_path = sentence_transformers.util.load_dir_path(
+                    model_name_or_path,
+                    module_config['path'],
+                    token,
+                    cache_folder,
+                    revision=revision,
+                )
                 module = module_class.load(module_path)
             modules[module_config['name']] = module
 
