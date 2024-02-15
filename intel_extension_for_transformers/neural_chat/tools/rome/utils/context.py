8dcf0ea77c07ea4cf8d4e2010ad3b3dbc6f36bcb
XinyuYe-Intel <xinyu.ye@intel.com>
2024-02-15 16:17:22 +0800

    [NeuralChat] Add ROME implementation and example (#1231)
    * added rome implemetation and example.
    
    Signed-off-by: Ye, Xinyu <xinyu.ye@intel.com>

---
+++ intel_extension_for_transformers/neural_chat/tools/rome/utils/context.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CONTEXT_TEMPLATES = [
+    "{}",
+    "Human: {}",
+    "User: {}",
+    "Hello world. {}",
+    "I love China! {}",
+    "I am an AI assistant. {}",
+    "How about the weather today? {}",
+    "The cat sat on the mat. {}",
+    "I went to the store today. {}",
+    "The sun shines brightly in summer. {}",
+    "He loves to play the guitar. {}",
+    "She smiled and waved goodbye. {}",
+    "用户：{}",
+    "你好，世界。{}",
+    "我爱中国！{}",
+    "我是一个人工智能助手。{}",
+    "今天的天气怎么样？{}",
+    "猫坐在垫子上。{}",
+    "我今天去了商店。{}",
+    "夏天太阳很刺眼。{}",
+    "他喜欢弹吉他。{}",
+    "她微笑着挥手道别。{}"
+]
