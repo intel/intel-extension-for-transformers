695d149106c1acf79e8a31f3d71153ebe7a83b9f
XuhuiRen <xuhui.ren@intel.com>
2024-02-20 15:53:51 +0800

    add reranker model path
    Signed-off-by: XuhuiRen <xuhui.ren@intel.com>

--- intel_extension_for_transformers/neural_chat/pipeline/plugins/retrieval/retriever_adapter.py
+++ intel_extension_for_transformers/neural_chat/pipeline/plugins/retrieval/retriever_adapter.py
@@ -32,9 +32,10 @@ class RetrieverAdapter():
     """Retrieve the document database with Chroma database using dense retrieval."""
 
     def __init__(self, retrieval_type='default', document_store=None, child_document_store=None, docs=None,  \
-                 reranker_model=None, top_n = 1, enable_rerank = False, **kwargs):
+                 reranker_model="BAAI/bge-reranker-large", top_n = 1, enable_rerank = False, **kwargs):
         self.retrieval_type = retrieval_type
         if enable_rerank:
+            from intel_extension_for_transformers.langchain.retrievers import BgeReranker
             self.reranker = BgeReranker(model_name = reranker_model, top_n=top_n)
         else:
             self.reranker = None
