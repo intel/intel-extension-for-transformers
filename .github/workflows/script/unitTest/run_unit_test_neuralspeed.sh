7b3abca210a8f21c2b0b33ec00d867bdaab3145a
VincyZhang <wenxin.zhang@intel.com>
2024-02-26 13:55:45 +0800

    upgrade pytorch/ipex to 2.2.0 (#1280)
    Co-authored-by: Wang, Chang <chang1.wang@intel.com>

--- .github/workflows/script/unitTest/run_unit_test_neuralspeed.sh
+++ .github/workflows/script/unitTest/run_unit_test_neuralspeed.sh
@@ -24,8 +24,6 @@ function neuralspeed_test() {
         [ $(grep -c "==ERROR:" ${ut_log_name}) != 0 ] ||
         [ $(grep -c "ModuleNotFoundError:" ${ut_log_name}) != 0 ] ||
         [ $(grep -c "ImportError:" ${ut_log_name}) != 0 ]; then
-        exit 1
-    fi; then
         $BOLD_RED && echo "Find errors in engine test, please check the output..." && $RESET
         exit 1
     else
