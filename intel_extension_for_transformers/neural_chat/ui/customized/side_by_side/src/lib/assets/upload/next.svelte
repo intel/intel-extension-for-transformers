5835c3873e366f6d324d75bc0cf7a954451973c7
WenjiaoYue <108783334+WenjiaoYue@users.noreply.github.com>
2024-03-04 20:44:49 +0800

    customized side by side (#1329)
    * add customized side by side project into ITREX
    
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>

---
+++ intel_extension_for_transformers/neural_chat/ui/customized/side_by_side/src/lib/assets/upload/next.svelte
@@ -0,0 +1,16 @@
+<svg
+	class="h-4 w-4 text-white rtl:rotate-180 dark:text-white-800"
+	aria-hidden="true"
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 6 10"
+>
+	<path
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+		d="m1 9 4-4-4-4"
+	/>
+</svg>
+
