7e852fda5ec094c67fae90247b8ff2e56e85dffb
Yu Zhentao <zhentao.yu@intel.com>
2024-03-18 01:24:47 -0700

    fix optimize ut
    Signed-off-by: Yu Zhentao <zhentao.yu@intel.com>

--- intel_extension_for_transformers/neural_chat/tests/ci/utils/test_device_utils.py
+++ intel_extension_for_transformers/neural_chat/tests/ci/utils/test_device_utils.py
@@ -21,13 +21,13 @@ import torch
 class TestDevice(unittest.TestCase):
     def test_cpu(self):
         torch.cuda.is_available = MagicMock(return_value=False)
-        from intel_extension_for_transformers.utils.device_utils import is_hpu_available, get_device_type
+        from intel_extension_for_transformers.neural_chat.utils.common import is_hpu_available, get_device_type
         device = get_device_type()
         self.assertTrue("cpu" in device)
 
     def test_gpu(self):
         torch.cuda.is_available = MagicMock(return_value=True)
-        from intel_extension_for_transformers.utils.device_utils import is_hpu_available, get_device_type
+        from intel_extension_for_transformers.neural_chat.utils.common import is_hpu_available, get_device_type
         device = get_device_type()
         self.assertTrue("cuda" in device)
 
