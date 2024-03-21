17526844b731644d98f0adcd812b1cbe80a335a8
Liangyx2 <yuxiang.liang@intel.com>
2024-03-21 16:43:00 +0800

    Update test_evaluation.py
    Signed-off-by: Liangyx2 <yuxiang.liang@intel.com>

--- intel_extension_for_transformers/neural_chat/tests/nightly/tools/test_evaluation.py
+++ intel_extension_for_transformers/neural_chat/tests/nightly/tools/test_evaluation.py
@@ -41,9 +41,15 @@ class TestEvaluation(unittest.TestCase):
         return super().tearDown()
 
     def test_retrieval_dataset_construction(self):
+        path = \
+          "/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/assets/docs/retrieve_multi_doc/"
+        if os.path.exists(path):
+            input_path=path
+        else:
+            input_path='../assets/docs/retrieve_multi_doc/'
         argv = ['--llm_model', '/tf_dataset2/models/nlp_toolkit/neural-chat-7b-v3-1', \
                 '--embedding_model', '/tf_dataset2/inc-ut/gte-base', \
-                '--input', '/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/assets/docs/retrieve_multi_doc/', \
+                '--input', input_path, \
                 '--output', './data', \
                 '--range_for_sampling', '2-2', \
                 '--negative_number', '1']
@@ -52,24 +58,54 @@ class TestEvaluation(unittest.TestCase):
             self.assertTrue(os.path.exists("./data/minedHN_split.jsonl"))
 
     def test_llm_generate_truth(self):
+        path = \
+          "/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/example.jsonl"
+        if os.path.exists(path):
+            input_path=path
+        else:
+            input_path='../tools/evaluation/data_augmentation/example.jsonl'
         argv = ['--llm_model', '/tf_dataset2/models/nlp_toolkit/neural-chat-7b-v3-1', \
-                '--input', '/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/example.jsonl', \
+                '--input', input_path, \
                 '--output', 'ground_truth.jsonl']
         with patch('sys.argv', ['python llm_generate_truth.py'] + argv):
             llm_generate_truth.main()
             self.assertTrue(os.path.exists("ground_truth.jsonl"))
 
     def test_evaluate_retrieval(self):
-        argv = ['--index_file_jsonl_path', '/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/candidate_context.jsonl', \
-                '--query_file_jsonl_path', '/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/example.jsonl', \
+        path1 = \
+          "/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/candidate_context.jsonl"
+        if os.path.exists(path1):
+            index_file_jsonl_path=path1
+        else:
+            index_file_jsonl_path='../tools/evaluation/data_augmentation/candidate_context.jsonl'
+        path2 = \
+          "/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/example.jsonl"
+        if os.path.exists(path2):
+            query_file_jsonl_path=path2
+        else:
+            query_file_jsonl_path='../tools/evaluation/data_augmentation/example.jsonl'
+        argv = ['--index_file_jsonl_path', index_file_jsonl_path, \
+                '--query_file_jsonl_path', query_file_jsonl_path, \
                 '--embedding_model', '/tf_dataset2/inc-ut/gte-base']
         with patch('sys.argv', ['python evaluate_retrieval.py'] + argv):
             result = evaluate_retrieval.main()
             self.assertIsNotNone(result)
 
     def test_ragas_evaluation(self):
-        argv = ['--answer_file', '/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/answer.jsonl', \
-                '--ground_truth_file', '/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/ground_truth.jsonl', \
+        path1 = \
+          "/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/answer.jsonl"
+        if os.path.exists(path1):
+            answer_file_path=path1
+        else:
+            answer_file_path='../tools/evaluation/data_augmentation/answer.jsonl'
+        path2 = \
+          "/intel-extension-for-transformers/intel_extension_for_transformers/neural_chat/tools/evaluation/data_augmentation/ground_truth.jsonl"
+        if os.path.exists(path2):
+            ground_truth_file_path=path2
+        else:
+            ground_truth_file_path='../tools/evaluation/data_augmentation/ground_truth.jsonl'
+        argv = ['--answer_file', answer_file_path, \
+                '--ground_truth_file', ground_truth_file_path, \
                 '--llm_model', '/tf_dataset2/models/nlp_toolkit/neural-chat-7b-v3-1', \
                 '--embedding_model', '/tf_dataset2/inc-ut/gte-base']
         with patch('sys.argv', ['python ragas_evaluation.py'] + argv):
