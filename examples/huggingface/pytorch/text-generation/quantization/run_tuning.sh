5458e1585a65efc63c4caf7d913179befede5db6
Wenxin Zhang <wenxin.zhang@intel.com>
2024-04-01 11:25:34 +0800

    use int4 instead int_clip for ns
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- examples/huggingface/pytorch/text-generation/quantization/run_tuning.sh
+++ examples/huggingface/pytorch/text-generation/quantization/run_tuning.sh
@@ -211,30 +211,36 @@ function run_tuning {
         extra_cmd=$extra_cmd" --trust_remote_code"
     elif [ "${topology}" = "mistral_7b_int4_autoround" ]; then
         model_name_or_path="/tf_dataset2/models/pytorch/Mistral-7B-v0.1"
-        extra_cmd=$extra_cmd" --woq --bits 4 --weight_dtype int4_clip --compute_dtype fp32 --scheme asym "
+        extra_cmd=$extra_cmd" --woq --bits 4 --compute_dtype fp32 --scheme asym "
         extra_cmd=$extra_cmd" --woq_algo "AutoRound" --desc_act --blocksize 128 --max_input_length 2048 "
         extra_cmd=$extra_cmd" --output_dir ${tuned_checkpoint}"
         extra_cmd=$extra_cmd" --trust_remote_code"
         if [[ $backend == "neuralspeed" ]]; then
-            extra_cmd=$extra_cmd" --use_neural_speed"
+            extra_cmd=$extra_cmd" --use_neural_speed --weight_dtype int4"
+        else
+            extra_cmd=$extra_cmd"--weight_dtype int4_clip"
         fi
     elif [ "${topology}" = "mistral_7b_int4_rtn" ]; then
         model_name_or_path="/tf_dataset2/models/pytorch/Mistral-7B-v0.1"
-        extra_cmd=$extra_cmd" --woq --bits 4 --weight_dtype int4_clip --compute_dtype fp32 --scheme asym "
+        extra_cmd=$extra_cmd" --woq --bits 4 -compute_dtype fp32 --scheme asym "
         extra_cmd=$extra_cmd" --woq_algo "Rtn" --desc_act --blocksize 128 --max_input_length 2048 "
         extra_cmd=$extra_cmd" --output_dir ${tuned_checkpoint}"
         extra_cmd=$extra_cmd" --trust_remote_code"
         if [[ $backend == "neuralspeed" ]]; then
-            extra_cmd=$extra_cmd" --use_neural_speed"
+            extra_cmd=$extra_cmd" --use_neural_speed --weight_dtype int4"
+        else
+            extra_cmd=$extra_cmd"--weight_dtype int4_clip"
         fi
     elif [ "${topology}" = "mistral_7b_int4_gptq" ]; then
         model_name_or_path="/tf_dataset2/models/pytorch/Mistral-7B-v0.1"
-        extra_cmd=$extra_cmd" --woq --bits 4 --weight_dtype int4_clip --compute_dtype fp32 --scheme asym "
+        extra_cmd=$extra_cmd" --woq --bits 4 --compute_dtype fp32 --scheme asym "
         extra_cmd=$extra_cmd" --woq_algo "GPTQ" --desc_act --blocksize 128 --max_input_length 2048 "
         extra_cmd=$extra_cmd" --output_dir ${tuned_checkpoint}"
         extra_cmd=$extra_cmd" --trust_remote_code"
         if [[ $backend == "neuralspeed" ]]; then
-            extra_cmd=$extra_cmd" --use_neural_speed"
+            extra_cmd=$extra_cmd" --use_neural_speed --weight_dtype int4"
+        else
+            extra_cmd=$extra_cmd"--weight_dtype int4_clip"
         fi
     fi
 
