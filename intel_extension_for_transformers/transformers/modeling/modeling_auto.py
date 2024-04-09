a437b7098a688ca9cc66a5d8420b98698cf93826
Yu, Zhentao <zhentao.yu@intel.com>
2024-04-09 09:31:15 +0000

    clea model from_pretrained when use_neural_speed
    Signed-off-by: Yu, Zhentao <zhentao.yu@intel.com>

--- intel_extension_for_transformers/transformers/modeling/modeling_auto.py
+++ intel_extension_for_transformers/transformers/modeling/modeling_auto.py
@@ -420,6 +420,28 @@ class _BaseQBitsAutoModelClass:
                     assert quantization_config.get("quant_method", None) in ConfigInit, \
                         "Detect this model is not a low-bit model."
                     quantization_config = ConfigInit[quantization_config["quant_method"]].from_dict(quantization_config)
+                    logger.info("Loading Low Bits model by Neural Speed.")
+                    quantization_config.post_init_runtime()
+                    from neural_speed import Model
+
+                    model = Model()
+                    model.init( # pylint: disable=E1123
+                        pretrained_model_name_or_path,
+                        weight_dtype=quantization_config.weight_dtype,
+                        alg=quantization_config.scheme,
+                        group_size=quantization_config.group_size,
+                        scale_dtype=quantization_config.scale_dtype,
+                        compute_dtype=quantization_config.compute_dtype,
+                        use_ggml=quantization_config.use_ggml,
+                        use_quant=True,
+                        use_gptq=quantization_config.quant_method.value == "gptq"
+                        or quantization_config.quant_method.value == "autoround",
+                        use_awq=quantization_config.quant_method.value == "awq"
+                        or quantization_config.quant_method.value == "rtn",
+                        model_hub=model_hub,
+                    )
+                    model.quantization_config = quantization_config
+                    return model
             else:
                 logger.info(
                     "quantization_config: {}".format(config.quantization_config)
@@ -559,7 +581,10 @@ class _BaseQBitsAutoModelClass:
         ):
             logger.info("Applying Weight Only Quantization.")
             if use_neural_speed:
-                logger.info("Using Neural Speed.")
+                if not isinstance(quantization_config, RtnConfig):
+                    logger.error("Only Supports RTN Quantization in Neural Speed.")
+                    exit(0)
+                logger.info("Quantize model by Neural Speed with RTN Algorithm.")
                 quantization_config.post_init_runtime()
                 from neural_speed import Model
 
@@ -573,9 +598,8 @@ class _BaseQBitsAutoModelClass:
                     compute_dtype=quantization_config.compute_dtype,
                     use_ggml=quantization_config.use_ggml,
                     use_quant=True,
-                    use_gptq=quantization_config.quant_method.value == "gptq"
-                    or quantization_config.quant_method.value == "autoround",
-                    use_awq=quantization_config.quant_method.value == "awq",
+                    use_gptq=False,
+                    use_awq=False,
                     model_hub=model_hub,
                 )
                 model.quantization_config = quantization_config
@@ -939,17 +963,33 @@ class _BaseQBitsAutoModelClass:
             )
             logger.info("SmoothQuant done.")
         else:
-            model = cls.ORIG_MODEL.from_pretrained(
-                pretrained_model_name_or_path, *model_args, config=config, **kwargs
-            )
-            if (
-                not torch.cuda.is_available()
-                or device_map == "cpu"
-                or device_map == torch.device("cpu")
-            ) and model.config.model_type == "chatglm":
-                model = model.float()
+            if use_neural_speed:
+                logger.info("Using Neural Speed with FP32 model dtype.")
+                from neural_speed import Model
 
-            model.eval()
+                model = Model()
+                model.init( # pylint: disable=E1123
+                    pretrained_model_name_or_path,
+                    weight_dtype="fp32",
+                    use_quant=False,
+                    use_gptq=False,
+                    use_awq=False,
+                    model_hub=model_hub,
+                )
+                model.quantization_config = None
+                return model
+            else:
+                model = cls.ORIG_MODEL.from_pretrained(
+                    pretrained_model_name_or_path, *model_args, config=config, **kwargs
+                )
+                if (
+                    not torch.cuda.is_available()
+                    or device_map == "cpu"
+                    or device_map == torch.device("cpu")
+                ) and model.config.model_type == "chatglm":
+                    model = model.float()
+
+                model.eval()
         return model
 
     @classmethod
