57a65ec42e0a37c07f35c1d4984eb011906ef932
Meng, Hengyu <hengyu.meng@intel.com>
2024-04-16 19:13:25 +0800

    Update run_generation_gpu_woq.py
    Signed-off-by: Meng, Hengyu <hengyu.meng@intel.com>

--- examples/huggingface/pytorch/text-generation/quantization/run_generation_gpu_woq.py
+++ examples/huggingface/pytorch/text-generation/quantization/run_generation_gpu_woq.py
@@ -249,7 +249,9 @@ if args.benchmark:
         dtype=amp_dtype if amp_enabled else None,
     ):
         for i in range(num_iter + num_warmup):
-            with torch.autograd.profiler_legacy.profile(enabled=args.do_profiling, use_xpu=(args.device=="xpu"), record_shapes=False) as prof:
+            # workaround for Windows
+            # with torch.autograd.profiler_legacy.profile(enabled=args.do_profiling, use_xpu=(args.device=="xpu"), record_shapes=False) as prof:
+            if True
                 input_ids = tokenizer(
                     prompt, return_tensors="pt").input_ids.to(args.device)
                 tic = time.time()
