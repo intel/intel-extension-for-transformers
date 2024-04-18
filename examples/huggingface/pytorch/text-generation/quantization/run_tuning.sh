0c8da49c942cc8d6aac76fae8ee6b9e550c4f62f
Wenxin Zhang <wenxin.zhang@intel.com>
2024-04-18 09:22:59 +0800

    update for woq tuning scripts
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- examples/huggingface/pytorch/text-generation/quantization/run_tuning.sh
+++ examples/huggingface/pytorch/text-generation/quantization/run_tuning.sh
@@ -63,12 +63,15 @@ function run_tuning {
     elif [ "${topology}" = "gpt_j_woq_rtn" ]; then
         model_name_or_path="/tf_dataset2/models/pytorch/gpt-j-6B"
         extra_cmd=$extra_cmd" --woq"
+        extra_cmd=$extra_cmd" --weight_dtype ${weight_dtype}"
     elif [ "${topology}" = "gpt_j_woq_bab" ]; then
         model_name_or_path="/tf_dataset2/models/pytorch/gpt-j-6B"
         extra_cmd=$extra_cmd" --bitsandbytes"
+        extra_cmd=$extra_cmd" --weight_dtype ${weight_dtype}"
     elif [ "${topology}" = "gpt_j_woq_load4bit" ]; then
         model_name_or_path="/tf_dataset2/models/pytorch/gpt-j-6B"
         extra_cmd=$extra_cmd" --load_in_4bit"
+        extra_cmd=$extra_cmd" --weight_dtype ${weight_dtype}"
     elif [ "${topology}" = "gpt_j_woq_load8bit" ]; then
         model_name_or_path="/tf_dataset2/models/pytorch/gpt-j-6B"
         extra_cmd=$extra_cmd" --load_in_8bit "
