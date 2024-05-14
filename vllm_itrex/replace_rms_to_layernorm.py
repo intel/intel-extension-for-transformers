569e46748baddc2ebb467c4163bb16521c422c69
Zhenzhong1 <zhenzhong.xu@intel.com>
2024-05-13 19:53:46 -0700

    replace more linears done

--- vllm_itrex/replace_rms_to_layernorm.py
+++ vllm_itrex/replace_rms_to_layernorm.py
@@ -3,6 +3,8 @@ from torch.nn import LayerNorm
 import torch
 import copy
 
+import time
+
 from neural_compressor import quantization
 from intel_extension_for_transformers.transformers.utils import RtnConfig
 from intel_extension_for_transformers.transformers.llm.quantization.utils import convert_to_quantized_model
@@ -10,14 +12,26 @@ from intel_extension_for_transformers.transformers.llm.quantization.utils import
 
 # 为什么一定要放在上面？？
 original_linear = torch.nn.Linear(in_features=4096,out_features=4608, bias=True, dtype=torch.bfloat16)
+dense_linear = torch.nn.Linear(in_features=4096,out_features=4096, bias=False, dtype=torch.bfloat16)
+dense_h_to_4h = torch.nn.Linear(in_features=4096,out_features=27392, bias=False, dtype=torch.bfloat16)
+dense_4h_to_h = torch.nn.Linear(in_features=13696,out_features=4096, bias=False, dtype=torch.bfloat16)
+# output_layer = torch.nn.Linear(in_features=4096,out_features=65024, bias=False, dtype=torch.bfloat16)
 # original_linear = torch.nn.Linear(in_features=4096,out_features=4608, bias=True, dtype=torch.float32)
 
 # nn.Linear
 #prompts = ["Hello, my name is", "The capital of France is", "你好"]  # Sample prompts.
-#prompts = ["hello, my name is"]
 prompts = ["你好"]
+#prompts = ["你好？请问是生病了怎么办？我到底是得了什么毛病。明天我可以不上学嘛？我不想上课，请问所以我应该如何跟老师说？"]
+#prompts = ["once upon a time, a little girl"]
 llm = LLM(model="/home/zhenzhong/model/chatglm2-6b", trust_remote_code=True)  # Create an LLM.
 
+T1 = time.time()
+outputs = llm.generate(prompts)  # Generate texts from the prompts.
+T2 = time.time()
+print("tokens_length) = ", len(outputs[0].prompt_token_ids))
+print('original model time cost:%s ms' % ((T2 - T1)*1000))
+
+print(outputs)
 
 from vllm.model_executor.model_loader import get_model, get_model_loader, get_model_architecture
 
@@ -34,11 +48,12 @@ print("Original model ----------------------------------------------------------
 # model._modules['transformer']._modules['encoder']._modules['final_layernorm'] = copy.deepcopy(layer_norm_func)
 # print("modified model ---------------------------------------------------------------------------------= ", model)
 
-
-
 for idx in range(28):
     print("modified!!!-----------------", idx)
-    model._modules['transformer']._modules['encoder']._modules['layers']._modules[str(idx)]._modules['self_attention']._modules['query_key_value'] = copy.deepcopy(original_linear)    
+    model._modules['transformer']._modules['encoder']._modules['layers']._modules[str(idx)]._modules['self_attention']._modules['query_key_value'] = copy.deepcopy(original_linear)
+    model._modules['transformer']._modules['encoder']._modules['layers']._modules[str(idx)]._modules['self_attention']._modules['dense'] = copy.deepcopy(dense_linear)
+    model._modules['transformer']._modules['encoder']._modules['layers']._modules[str(idx)]._modules['mlp']._modules['dense_h_to_4h'] = copy.deepcopy(dense_h_to_4h)   
+    model._modules['transformer']._modules['encoder']._modules['layers']._modules[str(idx)]._modules['mlp']._modules['dense_4h_to_h'] = copy.deepcopy(dense_4h_to_h)
 print("modified model ---------------------------------------------------------------------------------= ", model)
 
 # load weights
@@ -46,30 +61,19 @@ loader = get_model_loader(llm.llm_engine.load_config)
 model.load_weights(loader._get_weights_iterator(llm.llm_engine.model_config.model, llm.llm_engine.model_config.revision, fall_back_to_pt=True))
 
 
-
 config = RtnConfig(compute_dtype="fp32", group_size=128, scale_dtype="fp32", weight_dtype="int4_clip", bits=4)
-
 model = convert_to_quantized_model(model, config)
 
-# inc_model = quantization.fit(model, config)
-
-###
-# inc_model = quantization.fit(
-#     model, conf, calib_func=calib_func, calib_dataloader=calib_dataloader
-# )
-
-
-
-# import pdb;pdb.set_trace()
-
 # inference
-import time
-T1 = time.time()
+T3 = time.time()
 
 outputs = llm.generate(prompts)  # Generate texts from the prompts.
 
-T2 = time.time()
-print('程序运行时间:%s毫秒' % ((T2 - T1)*1000))
+T4 = time.time()
+print("input_tokens_length) = ", len(outputs[0].prompt_token_ids))
+print("output_tokens_length) = ", len(outputs[0].outputs[0].token_ids))
+print('qbits    model time cost:%2s ms' % ((T4 - T3)*1000))
+print('original model time cost:%2s ms' % ((T2 - T1)*1000))
 print(outputs)
 
 
