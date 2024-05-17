36a8b977f38cfc44d367229ed9d12cc31e3e6e0c
Yu, Zhentao <zhentao.yu@intel.com>
2024-05-17 06:18:51 +0000

    update scripts
    Signed-off-by: Yu, Zhentao <zhentao.yu@intel.com>

--- examples/habana/streaming_llm/run_yi_34b_200k_bf16_streaming.sh
+++
@@ -1,8 +0,0 @@
-python run_streaming_llm.py \
-    --model_name_or_path=01-ai/Yi-34B-200K \
-    --dataset_name=HuggingFaceH4/mt_bench_prompts \
-    --trust_remote_code \
-    --window_size=8000 \
-    --attention_sink_size=4 \
-    --max_new_token=1000 \
-    --enable_streaming
---
+++ examples/habana/streaming_llm/run_yi_34b_bf16_streaming.sh
@@ -0,0 +1,10 @@
+python run_streaming_llm.py \
+    --model_name_or_path=01-ai/Yi-34B \
+    --dataset=HuggingFaceH4/mt_bench_prompts \
+    --attention_sink_window_size=1020 \
+    --attention_sink_size=4 \
+    --max_new_token=512 \
+    --num_sample=-1 \
+    --bf16 \
+    --use_kv_cache \
+    --use_hpu_graphs
