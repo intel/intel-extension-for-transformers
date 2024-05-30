42d803a9ea19f474f0c69ce24d4a6d4a9a2bc27b
Liangyx2 <yuxiang.liang@intel.com>
2024-05-30 09:48:58 +0800

    Update retrieval_agent.py
    Signed-off-by: Liangyx2 <yuxiang.liang@intel.com>

--- intel_extension_for_transformers/neural_chat/pipeline/plugins/retrieval/retrieval_agent.py
+++ intel_extension_for_transformers/neural_chat/pipeline/plugins/retrieval/retrieval_agent.py
@@ -321,7 +321,7 @@ class Agent_QA():
                 logging.info("Chat with QA Agent.")
                 if len(context) == 0:
                     return "Response with template.", links
-                prompt = generate_qa_enterprise(query, context)
+                prompt = generate_qa_enterprise(query, context, template=self.response_template)
         elif self.mode == "general":
         # For general setting, will return top-k documents.
             if 'qa' not in intent.lower() and context == '':
@@ -332,7 +332,7 @@ class Agent_QA():
                 context, links = self.retriever.get_context(query)
                 if len(context) == 0:
                     return "Response with template.", links
-                prompt = generate_qa_prompt(query, context)
+                prompt = generate_qa_prompt(query, context, template=self.response_template)
         else:
             logging.error("The selected generation mode is invalid!")
         links = list(set(links))
